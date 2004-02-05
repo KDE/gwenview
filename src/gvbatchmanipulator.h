1e416fec383196c1ecc875962df883bcdfccc4f9
Aurélien Gâteau <agateau@kde.org>
2004-02-05 09:20:56 +0000

    Added missing files
    svn path=/trunk/kdeextragear-1/gwenview/; revision=285235

---
+++ src/gvbatchmanipulator.h
@@ -0,0 +1,54 @@
+// vim: set tabstop=4 shiftwidth=4 noexpandtab
+/*
+Gwenview - A simple image viewer for KDE
+Copyright 2000-2004 Aurélien Gâteau
+ 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ 
+*/
+
+
+#ifndef GVBATCHMANIPULATOR_H
+#define GVBATCHMANIPULATOR_H   
+
+// Qt includes
+#include <qwidget.h>
+
+// KDE includes
+#include <kurl.h>
+
+// Local includes
+#include "gvimageutils.h"
+
+class GVBatchManipulatorPrivate;
+
+class GVBatchManipulator : public QObject {
+Q_OBJECT
+public:
+	GVBatchManipulator(QWidget* parent, const KURL::List&, GVImageUtils::Orientation);
+	~GVBatchManipulator();
+	void apply();
+
+public slots:
+	void slotImageLoaded();
+
+signals:
+	void imageModified(const KURL& url);
+
+private:
+	GVBatchManipulatorPrivate* d;
+};
+
+#endif /* GVBATCHMANIPULATOR_H */
