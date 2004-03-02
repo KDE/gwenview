560c0466f75d6c0f92b15f8327ddfd539204c934
Jonathan Riddell <jr@jriddell.org>
2004-03-02 23:52:49 +0000

    Don't include kdeprint/kprintdialog.h It's an internal header which complains about being included and it doesn't seem to be used.
    svn path=/trunk/kdeextragear-1/gwenview/; revision=293243

--- src/gvmainwindow.cpp
+++ src/gvmainwindow.cpp
@@ -57,7 +57,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <kurlcompletion.h>
 #include <kurlrequesterdlg.h>
 #include <kprinter.h>
-#include <kdeprint/kprintdialog.h>
 
 // Local
 #include "fileoperation.h"
