af8431a966cd7ee868d252c2aefb9af557896776
Aurélien Gâteau <agateau@kde.org>
2004-04-20 21:58:33 +0000

    Added missing include for KIPI support.
    svn path=/trunk/kdeextragear-1/gwenview/; revision=305266

--- src/gvmainwindow.cpp
+++ src/gvmainwindow.cpp
@@ -58,6 +58,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <kurlrequesterdlg.h>
 #include <kprinter.h>
 
+#include <config.h>
 // KIPI
 #ifdef HAVE_KIPI
 #include <libkipi/plugin.h>
