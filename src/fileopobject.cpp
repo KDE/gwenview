c07f8f7d8a7e390c37e88b0dc7b6228b16a01173
Aurélien Gâteau <agateau@kde.org>
2004-05-09 11:52:46 +0000

    Removed inclusion of kdirselectdialog.h since it's not used anymore.
    svn path=/trunk/kdeextragear-1/gwenview/; revision=309720

--- src/fileopobject.cpp
+++ src/fileopobject.cpp
@@ -23,11 +23,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <qfile.h>
 #include <qstylesheet.h>
 #include <qwidget.h>
-#include <qpopupmenu.h>
 
 // KDE 
 #include <kdeversion.h>
-#include <kdirselectdialog.h>
 #include <kfiledialog.h>
 #include <kfilefiltercombo.h>
 #include <kglobalsettings.h>
