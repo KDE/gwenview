86c59f0041f601d3a28dd4454171e78b3cf23c56
Brad Hards <bradh@frogmouth.net>
2004-05-09 10:09:15 +0000

    Add missing #include of <qpopupmenu.h>. Required for <kdirselectdialog.h>
    svn path=/trunk/kdeextragear-1/gwenview/; revision=309704

--- src/fileopobject.cpp
+++ src/fileopobject.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <qfile.h>
 #include <qstylesheet.h>
 #include <qwidget.h>
+#include <qpopupmenu.h>
 
 // KDE 
 #include <kdeversion.h>
