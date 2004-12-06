2b7e1088cd7a8490e2c248e30436b1e8e1f1da78
Luboš Luňák <l.lunak@kde.org>
2004-12-06 14:50:29 +0000

    Try to generate thumbnails from the currently selected thumbnail, not from the first visible item. Makes browsing thumbnails using keyboard feel slightly better. I tried also more complicated algorithms, but they visually were weird.
    svn path=/trunk/kdeextragear-1/gwenview/; revision=368903

--- src/gvfilethumbnailview.cpp
+++ src/gvfilethumbnailview.cpp
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <qpixmapcache.h>
 #include <qpushbutton.h>
 #include <qtimer.h>
+#include <qvaluevector.h>
 
 // KDE
 #include <kapplication.h>
@@ -158,6 +159,8 @@ GVFileThumbnailView::GVFileThumbnailView(QWidget* parent)
 		this, SLOT(slotDropped(QDropEvent*)) );
 	connect(this, SIGNAL( contentsMoving( int, int )),
 		this, SLOT( slotContentsMoving( int, int )));
+	connect(this, SIGNAL(currentChanged(QIconViewItem*)),
+		this, SLOT(slotCurrentChanged(QIconViewItem*)) );
 
 	QIconView::setSelectionMode(Extended);
 
@@ -257,12 +260,13 @@ void GVFileThumbnailView::startThumbnailUpdate() {
 void GVFileThumbnailView::doStartThumbnailUpdate(const KFileItemList* list) {
 	GVBusyLevelManager::instance()->setBusyLevel( this, BUSY_THUMBNAILS );
 
-	QValueList<const KFileItem*> imageList;
+	QValueVector<const KFileItem*> imageList;
+	imageList.reserve( list->count());
 	QPtrListIterator<KFileItem> it(*list);
 	for (;it.current(); ++it) {
 		KFileItem* item=it.current();
 		if (!item->isDir() && !GVArchive::fileItemIsArchive(item)) {
-			imageList << item;
+			imageList.append( item );
 		}
 	}
 	if (imageList.empty()) return;
@@ -577,22 +581,47 @@ void GVFileThumbnailView::slotClicked(QIconViewItem* iconItem) {
 	}
 }
 
-/**
- * when generating thumbnails, make the first visible (not loaded yet)
- * thumbnail be the next one processed by the thumbnail job
- */
 void GVFileThumbnailView::slotContentsMoving( int x, int y ) {
+	updateVisibilityInfo( x, y ); // use x,y, the signal is emitted before moving
+}
+
+void GVFileThumbnailView::slotCurrentChanged(QIconViewItem*) {
+	// trigger generating thumbnails from the current one
+	updateVisibilityInfo( contentsX(), contentsY());
+}
+
+/**
+ * when generating thumbnails, make the current thumbnail
+ * to be the next one processed by the thumbnail job, if visible,
+ * otherwise use the first visible thumbnail
+ */
+void GVFileThumbnailView::updateVisibilityInfo( int x, int y ) {
 	if (d->mThumbnailLoadJob.isNull()) return;
 	QRect r( x, y, visibleWidth(), visibleHeight());
-	GVFileThumbnailViewItem* pos = static_cast< GVFileThumbnailViewItem* >( findFirstVisibleItem( r ));
+	GVFileThumbnailViewItem* first = static_cast< GVFileThumbnailViewItem* >( findFirstVisibleItem( r ));
 	GVFileThumbnailViewItem* last = static_cast< GVFileThumbnailViewItem* >( findLastVisibleItem( r ));
-	while( pos != NULL ) {
+	for( GVFileThumbnailViewItem* pos = first;
+	     pos != last;
+	     pos = static_cast< GVFileThumbnailViewItem* >( pos->nextItem())) {
+		if( pos == currentItem()) {
+			KFileItem* fileItem = pos->fileItem();
+			if( fileItem ) {
+				d->mThumbnailLoadJob->setPriorityItems(fileItem,
+					first->fileItem(), last->fileItem());
+				return;
+			}
+			break;
+		}
+	}
+	for( GVFileThumbnailViewItem* pos = first;
+	     pos != last;
+	     pos = static_cast< GVFileThumbnailViewItem* >( pos->nextItem())) {
 		KFileItem* fileItem = pos->fileItem();
 		if( fileItem ) {
-			if( d->mThumbnailLoadJob->setNextItem(fileItem)) return;
+			d->mThumbnailLoadJob->setPriorityItems(fileItem,
+				first->fileItem(), last->fileItem());
+			return;
 		}
-		if( pos == last ) break;
-		pos = static_cast< GVFileThumbnailViewItem* >( pos->nextItem());
 	}
 }
 
