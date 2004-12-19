30223b2d3353be5d5f05f2e37d13894e5a5a23db
Aurélien Gâteau <agateau@kde.org>
2004-12-19 19:48:06 +0000

    Version 1.1.7
    svn path=/trunk/kdeextragear-1/gwenview/; revision=372023

--- gwenviewapp/main.cpp
+++ gwenviewapp/main.cpp
@@ -33,7 +33,7 @@ static KCmdLineOptions options[] = {
 	KCmdLineLastOption
 };
 
-static const char* version="CVS>=20041027";
+static const char* version="1.1.7";
 
 
 int main (int argc, char *argv[]) {
