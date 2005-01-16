4856d709c524371807244986b97c0ca812362ce5
Aurélien Gâteau <agateau@kde.org>
2005-01-16 00:41:19 +0000

    - Use KTempFile even for saving local files. - Better handling of symlinks. CCBUG: 96850
    svn path=/trunk/kdeextragear-1/gwenview/; revision=378910

--- src/gvdocumentloadedimpl.cpp
+++ src/gvdocumentloadedimpl.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <unistd.h>
 
 // Qt
+#include <qdir.h>
 #include <qfileinfo.h>
 #include <qtimer.h>
 
@@ -69,37 +70,72 @@ void GVDocumentLoadedImpl::transform(GVImageUtils::Orientation orientation) {
 }
 
 
-QString GVDocumentLoadedImpl::save(const KURL& url, const QCString& format) const {
+QString GVDocumentLoadedImpl::save(const KURL& _url, const QCString& format) const {
 	QString msg;
+	KURL url(_url);
+	
+	if (url.isLocalFile()) {
+		// If the file is a link, dereference it but take care of circular
+		// links
+		QFileInfo info(url.path());
+		if (info.isSymLink()) {
+			QStringList links;
+			while (info.isSymLink()) {
+				links.append(info.filePath());
+				QString path=info.readLink();
+				if (path[0]!='/') {
+					path=info.dirPath(true) + '/' + path;
+				}
+				path=QDir::cleanDirPath(path);
+				if (links.contains(path)) {
+					return i18n("<qt><b>Could not save the image to %1.</b><br/>This is a circular link.</qt>")
+					.arg(url.path());
+				}
+				info.setFile(path);
+			}
+			url.setPath(info.filePath());
+		}
+		
+		// Make some quick tests on the file if it is local
+		if (info.exists() && ! info.isWritable()) {
+			return
+				i18n("<qt><b>Could not save the image to %1.</b><br/>This file is read-only.</qt>")
+				.arg(info.filePath());
+		}
+		if (!info.exists()) {
+			QFileInfo parent=QFileInfo(info.dirPath());
+			if (!parent.isWritable()) {
+				return
+					i18n("<qt><b>Could not save the image to %1.</b><br/>The %2 folder is read-only.</qt>")
+					.arg(info.filePath())
+					.arg(parent.filePath());
+			}
+		}
+	}
 
-	KTempFile tmp;
+	// Save the file to a tmp file
+	QString prefix;
+	if (url.isLocalFile()) {
+		// We set the prefix to url.path() so that the temp file is one the
+		// same partition as the destination file. If we don't do this, rename
+		// will fail
+		prefix=url.path();
+	}
+	KTempFile tmp(prefix);
 	tmp.setAutoDelete(true);
-	QString path;
-	if (url.isLocalFile()) {
-		path=url.path() + ".gwenviewtmp";
-	} else {
-		path=tmp.name();
-	}
 
-	QFile file(path);
-	if (!file.open(IO_WriteOnly)) {
-		return
-			i18n("<qt><b>Could not save the image to %1.</b><br/>You are not allowed to save a file here.</qt>")
-			.arg(path);
-	}
-
-	msg=localSave(&file, format);
+	msg=localSave(tmp.file(), format);
 	if (!msg.isNull()) return msg;
-	setFileSize(QFileInfo(path).size());
+	setFileSize(QFileInfo(tmp.name()).size());
 
+	// Move the tmp file to the final dest
 	if (url.isLocalFile()) {
-		if( ::rename( QFile::encodeName( path ), QFile::encodeName( url.path())) < 0 ) {
-			::unlink( QFile::encodeName( path ));
+		if( ::rename( QFile::encodeName( tmp.name() ), QFile::encodeName( url.path())) < 0 ) {
 			return i18n("Could not save the image to %1").arg(url.path());
 		}
 	} else {
 		if (!KIO::NetAccess::upload(tmp.name(),url)) {
-			return i18n("Could not upload the file to %1").arg(path);
+			return i18n("Could not upload the file to %1").arg(url.prettyURL());
 		}
 	}
 
