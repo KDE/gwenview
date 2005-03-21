40a8dbe3fc5e575c546c66258ed89cc9a7b4b95e
Luboš Luňák <l.lunak@kde.org>
2005-03-21 15:51:47 +0000

    Ouch ... it was possible that the image was still in cache but file already wasn't. So reusing data from the cache meant there were no raw data for the image, needed e.g. for the JPEG operations. This causes those 'gwenview: ERROR: No data' messages, but it even lead to silent exists at my home computer (not on this other computer though).
    svn path=/trunk/kdeextragear-1/gwenview/; revision=399464

--- src/gvdocumentdecodeimpl.cpp
+++ src/gvdocumentdecodeimpl.cpp
@@ -229,6 +229,8 @@ public:
 	bool mWasFrameData;
 
 	GVImageFrames mFrames;
+
+	QCString mCachedFormat;
 };
 
 
@@ -269,6 +271,7 @@ void GVDocumentDecodeImpl::start() {
 	d->mFrames.clear();
 	d->mWasFrameData = false;
 	d->mNextFrameDelay = 0;
+	d->mCachedFormat = QCString();
 	KIO::Job* job=KIO::stat( mDocument->url(), false);
 	connect(job, SIGNAL(result(KIO::Job*)),
 		this, SLOT(slotStatResult(KIO::Job*)) );
@@ -294,10 +297,17 @@ void GVDocumentDecodeImpl::slotStatResult(KIO::Job* job) {
 		d->mRawData = GVCache::instance()->file( mDocument->url() );
 		GVImageFrames frames = GVCache::instance()->frames( mDocument->url(), format );
 		if( !frames.isEmpty()) {
-			setImageFormat(format);
-			d->mFrames = frames;
-			finish();
-			return;
+			if( !d->mRawData.isNull()) {
+				setImageFormat(format);
+				d->mFrames = frames;
+				finish();
+				return;
+			} else {
+				// the raw data is needed e.g. for JPEG, so it needs to be loaded
+				// if it's not in the cache
+				d->mCachedFormat = format;
+				d->mFrames = frames;
+			}
 		} else {
 			// Image in cache is broken, let's try the file
 			if( !d->mRawData.isNull()) {
@@ -333,6 +343,15 @@ void GVDocumentDecodeImpl::slotGetResult(KIO::Job* job) {
 	}
 
 	d->mGetComplete = true;
+
+	if( !d->mCachedFormat.isNull()) {
+		// Store raw data in cache
+		GVCache::instance()->addFile( mDocument->url(), d->mRawData, d->mTimestamp );
+		setImageFormat(d->mCachedFormat);
+		//d->mFrames = frames;
+		finish();
+		return;
+	}
 	
 	// Start the decoder thread if needed
 	if( d->mUseThread ) {
@@ -360,6 +379,11 @@ void GVDocumentDecodeImpl::slotDataReceived(KIO::Job*, const QByteArray& chunk) 
 
 void GVDocumentDecodeImpl::decodeChunk() {
 	if( d->mSuspended ) {
+		d->mDecoderTimer.stop();
+		return;
+	}
+
+	if( !d->mCachedFormat.isNull()) {
 		d->mDecoderTimer.stop();
 		return;
 	}
