3396ffbc73a696e6c713f3dbc8c6fb7aaf35f084
Aurélien Gâteau <agateau@kde.org>
2006-07-12 21:44:45 +0000

    Borrowed ClickLineEdit from the KDE-PIM project to reduce the width of the filter bar. Thanks kde-pim devs! CCMAIL:kde-pim@kde.org
    svn path=/trunk/extragear/graphics/gwenview/; revision=561674

---
+++ gvcore/clicklineedit.cpp
@@ -0,0 +1,106 @@
+/*
+   This file is part of libkdepim.
+   Copyright (c) 2004 Daniel Molkentin <molkentin@kde.org>
+   based on code by Cornelius Schumacher <schumacher@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "clicklineedit.h"
+
+#include "qpainter.h"
+
+namespace Gwenview {
+
+ClickLineEdit::ClickLineEdit(QWidget *parent, const char* name ) :
+        KLineEdit( parent, name )
+{
+    mDrawClickMsg = true;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// PUBLIC
+/////////////////////////////////////////////////////////////////////////////////////
+
+void ClickLineEdit::setClickMessage( const QString &msg )
+{
+    mClickMessage = msg;
+    repaint();
+}
+
+
+void ClickLineEdit::setText( const QString &txt )
+{
+    mDrawClickMsg = txt.isEmpty();
+    repaint();
+    KLineEdit::setText( txt );
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// PROTECTED
+/////////////////////////////////////////////////////////////////////////////////////
+
+//#include <kiconloader.h>
+void ClickLineEdit::drawContents( QPainter *p )
+{
+    KLineEdit::drawContents( p );
+
+    if ( mDrawClickMsg == true && !hasFocus() ) {
+        QPen tmp = p->pen();
+        p->setPen( palette().color( QPalette::Disabled, QColorGroup::Text ) );
+        QRect cr = contentsRect();
+
+        //p->drawPixmap( 3, 3, SmallIcon("filter") );
+
+        // Add two pixel margin on the left side
+        cr.rLeft() += 3;
+        p->drawText( cr, AlignAuto | AlignVCenter, mClickMessage );
+        p->setPen( tmp );
+    }
+}
+
+void ClickLineEdit::dropEvent( QDropEvent *ev )
+{
+    mDrawClickMsg = false;
+    KLineEdit::dropEvent( ev );
+}
+
+
+void ClickLineEdit::focusInEvent( QFocusEvent *ev )
+{
+    if ( mDrawClickMsg == true ) {
+        mDrawClickMsg = false;
+        repaint();
+    }
+    QLineEdit::focusInEvent( ev );
+}
+
+
+void ClickLineEdit::focusOutEvent( QFocusEvent *ev )
+{
+    if ( text().isEmpty() ) {
+        mDrawClickMsg = true;
+        repaint();
+    }
+    QLineEdit::focusOutEvent( ev );
+}
+} // namespace
+
+#include "clicklineedit.moc"
+
--- gvcore/fileviewcontroller.cpp
+++ gvcore/fileviewcontroller.cpp
@@ -53,6 +53,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // Local
 #include "archive.h"
 #include "cache.h"
+#include "clicklineedit.h"
 #include "cursortracker.h"
 #include "filedetailview.h"
 #include "fileoperation.h"
