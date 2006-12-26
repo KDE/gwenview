4fa83d5b3ca556e87ec39faea1520d8dfc6f25b0
Angelo Naselli <anaselli@linux.it>
2006-12-26 18:20:47 +0000

    Catched readMetadata exceptions
    BUG: 138467
    
    svn path=/trunk/extragear/graphics/gwenview/; revision=616769

--- imageutils/jpegcontent.cpp
+++ imageutils/jpegcontent.cpp
@@ -255,11 +255,11 @@ bool JPEGContent::loadFromData(const QByteArray& data) {
 	Exiv2::Image::AutoPtr image;
 	try {
 		image = Exiv2::ImageFactory::open((unsigned char*)data.data(), data.size());
+		image->readMetadata();
 	} catch (Exiv2::Error&) {
 		kdError() << "Could not load image with Exiv2\n";
 		return false;
 	}
-	image->readMetadata();
 
 	d->mExifData = image->exifData();
 	d->mComment = QString::fromUtf8( image->comment().c_str() );
