a07662e05a1cb2faaf5e70f4d638c1c6a3ec12e1
Aurélien Gâteau <agateau@kde.org>
2007-07-25 13:27:31 +0000

    Started to draw crop handles.
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=692395

--- lib/croptool.cpp
+++ lib/croptool.cpp
@@ -31,17 +31,55 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 // Local
 #include "imageview.h"
 
+static const int HANDLE_SIZE = 10;
+
 namespace Gwenview {
 
 
+enum CropHandle {
+	CH_Top = 1,
+	CH_Left = 2,
+	CH_Right = 4,
+	CH_Bottom = 8,
+	CH_TopLeft = CH_Top | CH_Left,
+	CH_BottomLeft = CH_Bottom | CH_Left,
+	CH_TopRight = CH_Top | CH_Right,
+	CH_BottomRight = CH_Bottom | CH_Right
+};
+
+
 struct CropToolPrivate {
+	CropTool* mCropTool;
 	QRect mRect;
+
+	QRect handleViewportRect(CropHandle handle) {
+		QRect viewportCropRect = mCropTool->imageView()->mapToViewport(mRect);
+		int left, top;
+		if (handle & CH_Top) {
+			top = viewportCropRect.top() - HANDLE_SIZE;
+		} else if (handle & CH_Bottom) {
+			top = viewportCropRect.bottom();
+		} else {
+			top = viewportCropRect.top() + (viewportCropRect.height() - HANDLE_SIZE) / 2;
+		}
+
+		if (handle & CH_Left) {
+			left = viewportCropRect.left() - HANDLE_SIZE;
+		} else if (handle & CH_Right) {
+			left = viewportCropRect.right();
+		} else {
+			left = viewportCropRect.left() + (viewportCropRect.width() - HANDLE_SIZE) / 2;
+		}
+
+		return QRect(left, top, HANDLE_SIZE, HANDLE_SIZE);
+	}
 };
 
 
 CropTool::CropTool(QObject* parent)
 : AbstractImageViewTool(parent)
 , d(new CropToolPrivate) {
+	d->mCropTool = this;
 }
 
 
@@ -59,6 +97,13 @@ void CropTool::setRect(const QRect& rect) {
 void CropTool::paint(QPainter* painter) {
 	QRect rect = imageView()->mapToViewport(d->mRect);
 	painter->drawRect(rect);
+
+	QList<CropHandle> lst;
+	lst << CH_Left << CH_Right << CH_Top << CH_Bottom << CH_TopLeft << CH_TopRight << CH_BottomLeft << CH_BottomRight;
+	Q_FOREACH(CropHandle handle, lst) {
+		rect = d->handleViewportRect(handle);
+		painter->fillRect(rect, Qt::black);
+	}
 }
 
 } // namespace
