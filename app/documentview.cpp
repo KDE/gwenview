89298809e7817eb191df3021bf01b353fecde7ce
Thomas Georgiou <tageorgiou@gmail.com>
2007-10-06 03:08:55 +0000

    Compile.  Thanks to hdh.
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=721771

--- app/documentview.cpp
+++ app/documentview.cpp
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <kparts/componentfactory.h>
 #include <kparts/statusbarextension.h>
 #include <kstatusbar.h>
+#include <kdebug.h>
 
 // Local
 #include <lib/imageviewpart.h>
--- app/thumbnailviewhelper.cpp
+++ app/thumbnailviewhelper.cpp
@@ -36,6 +36,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <kmenu.h>
 #include <kdebug.h>
 #include <kpropertiesdialog.h>
+#include <kdebug.h>
 
 // Local
 #include "fileopscontextmanageritem.h"
