989ae804e153e5852b72e8240646beb3be848343
David Faure <faure@kde.org>
2008-01-23 12:27:36 +0000

    fix compilation
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=765193

--- tests/documenttest.cpp
+++ tests/documenttest.cpp
@@ -33,6 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "../lib/imageutils.h"
 #include "testutils.h"
 
+#include <exiv2/exif.hpp>
+
 #include "documenttest.moc"
 
 QTEST_KDEMAIN( DocumentTest, GUI )
