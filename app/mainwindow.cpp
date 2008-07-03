b4155d46086b185b3d9b6d5655623d6a44f2f3ea
Aurélien Gâteau <agateau@kde.org>
2008-07-03 20:59:27 +0000

    When moving up in the folder hierarchy, keep the first child between the new url and the old url selected. BUG:158047
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=827815

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -812,6 +812,21 @@ void MainWindow::openDirUrl(const KUrl& url) {
 	if (url.equals(d->mDirModel->dirLister()->url(), KUrl::CompareWithoutTrailingSlash)) {
 		return;
 	}
+
+	KUrl currentUrl = d->mDirModel->dirLister()->url();
+	if (url.isParentOf(currentUrl)) {
+		// Keep first child between url and currentUrl selected
+		// If currentPath is      "/home/user/photos/2008/event"
+		// and wantedPath is      "/home/user/photos"
+		// pathToSelect should be "/home/user/photos/2008"
+		const QString currentPath = QDir::cleanPath(currentUrl.path(KUrl::RemoveTrailingSlash));
+		const QString wantedPath  = QDir::cleanPath(url.path(KUrl::RemoveTrailingSlash));
+		const QChar separator('/');
+		const int slashCount = wantedPath.count(separator);
+		const QString pathToSelect = currentPath.section(separator, 0, slashCount + 1);
+		d->mUrlToSelect = url;
+		d->mUrlToSelect.setPath(pathToSelect);
+	}
 	d->mDirModel->dirLister()->openUrl(url);
 	d->spreadCurrentDirUrl(url);
 	d->mDocumentView->reset();
