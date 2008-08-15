01ad52e7142da538dd09022eb3c2b86680de600c
Aurélien Gâteau <agateau@kde.org>
2008-08-15 22:01:00 +0000

    Unbreak test building
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=847651

--- tests/metadatabackendtest.cpp
+++ tests/metadatabackendtest.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 // Local
-#include "semanticinfobackendtest.moc"
+#include "metadatabackendtest.moc"
 
 // Qt
 #include <QSignalSpy>
