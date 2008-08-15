50802ffcfe41bb1d1d6c299cb153b738f027f46b
Aurélien Gâteau <agateau@kde.org>
2008-08-15 22:19:40 +0000

    SVN_SILENT: *.h => *.moc
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=847660

--- lib/semanticinfo/nepomuksemanticinfobackend.cpp
+++ lib/semanticinfo/nepomuksemanticinfobackend.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "nepomuksemanticinfobackend.h"
+#include "nepomuksemanticinfobackend.moc"
 
 // Qt
 #include <QMutex>
