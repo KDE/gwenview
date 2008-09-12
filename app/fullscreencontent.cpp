cb4c49c2fc787d5fdab7c775641135a19850f913
Aurélien Gâteau <agateau@kde.org>
2008-09-12 07:44:38 +0000

    #include .moc
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=860156

--- app/fullscreencontent.cpp
+++ app/fullscreencontent.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "fullscreencontent.h"
+#include "fullscreencontent.moc"
 
 // Qt
 #include <QAction>
