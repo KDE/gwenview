099dd01ea7ecc97e92f9c5e6bd0c5bd8e8137878
Aurélien Gâteau <agateau@kde.org>
2008-10-31 17:36:23 +0000

    Include .moc
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=878256

--- app/filtercontroller.cpp
+++ app/filtercontroller.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "filtercontroller.h"
+#include "filtercontroller.moc"
 
 #include <config-gwenview.h>
 
