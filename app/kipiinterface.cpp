46fa6327c2ab72033f6c243f98fd0cf64be9495d
Aurélien Gâteau <agateau@kde.org>
2008-11-28 08:52:10 +0000

    Cleanup
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=889996

--- app/kipiinterface.cpp
+++ app/kipiinterface.cpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include "kipiinterface.moc"
 
 // Qt
-#include <qlist.h>
+#include <QList>
 #include <QMenu>
 #include <QRegExp>
 
