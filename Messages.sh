12004812fadaa71fb348885d498fbe9137771e6c
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:52:50 +0000

    Extracting missing strings from .kcfg files.
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=951704

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$EXTRACTRC `find . -name "*.ui" -o -name "*.rc" ` >> rc.cpp
+$EXTRACTRC `find . -name "*.ui" -o -name "*.rc" -o -name "*.kcfg" ` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/gwenview.pot
