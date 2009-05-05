cd0976fa1a6b22c675a0f2898a843377eaf33aeb
Aurélien Gâteau <agateau@kde.org>
2009-05-05 08:06:14 +0000

    Fix broken include guard.
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=963674

--- lib/archiveutils.h
+++ lib/archiveutils.h
@@ -18,8 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
-
 #ifndef ARCHIVEUTILS_H
+#define ARCHIVEUTILS_H
 
 #include <lib/gwenviewlib_export.h>
 
