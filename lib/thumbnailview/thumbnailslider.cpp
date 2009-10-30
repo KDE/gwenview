0cfa349c023708d9b1283227cdfca686fa15a24e
Aurélien Gâteau <agateau@kde.org>
2009-10-30 22:34:46 +0000

    Oups, missing files
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=1042869

---
+++ lib/thumbnailview/thumbnailslider.cpp
@@ -0,0 +1,74 @@
+// vim: set tabstop=4 shiftwidth=4 noexpandtab:
+/*
+Gwenview: an image viewer
+Copyright 2009 Aurélien Gâteau <agateau@kde.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
+
+*/
+// Self
+#include "thumbnailslider.moc"
+
+// Qt
+#include <QSlider>
+#include <QToolTip>
+
+// KDE
+
+// Local
+
+namespace Gwenview {
+
+
+struct ThumbnailSliderPrivate {
+};
+
+
+ThumbnailSlider::ThumbnailSlider(QWidget* parent)
+: ZoomSlider(parent)
+, d(new ThumbnailSliderPrivate) {
+	connect(slider(), SIGNAL(actionTriggered(int)),
+		SLOT(slotActionTriggered(int)) );
+	slider()->setRange(48, 256);
+}
+
+
+ThumbnailSlider::~ThumbnailSlider() {
+	delete d;
+}
+
+
+void ThumbnailSlider::slotActionTriggered(int actionTriggered) {
+	updateToolTip();
+
+	if (actionTriggered != QAbstractSlider::SliderNoAction) {
+		// If we are updating because of a direct action on the slider, show
+		// the tooltip immediatly.
+		const QPoint pos = slider()->mapToGlobal(QPoint(0, slider()->height() / 2));
+		QToolTip::showText(pos, slider()->toolTip(), slider());
+	}
+}
+
+
+void ThumbnailSlider::updateToolTip() {
+	// FIXME: i18n?
+	const int size = slider()->sliderPosition();
+	const QString text = QString("%1 x %2").arg(size).arg(size);
+	slider()->setToolTip(text);
+}
+
+
+
+} // namespace
---
+++ lib/thumbnailview/thumbnailslider.h
@@ -0,0 +1,60 @@
+// vim: set tabstop=4 shiftwidth=4 noexpandtab:
+/*
+Gwenview: an image viewer
+Copyright 2009 Aurélien Gâteau <agateau@kde.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
+
+*/
+#ifndef THUMBNAILSLIDER_H
+#define THUMBNAILSLIDER_H
+
+#include <lib/gwenviewlib_export.h>
+
+// Qt
+#include <QObject>
+
+// KDE
+
+// Local
+#include <lib/zoomslider.h>
+
+namespace Gwenview {
+
+
+class ThumbnailSliderPrivate;
+/**
+ * A zoom slider which shows the thumbnail size as a tooltip when it is
+ * adjusted
+ */
+class GWENVIEWLIB_EXPORT ThumbnailSlider : public ZoomSlider {
+	Q_OBJECT
+public:
+	ThumbnailSlider(QWidget* parent=0);
+	~ThumbnailSlider();
+
+	void updateToolTip();
+
+private Q_SLOTS:
+	void slotActionTriggered(int actionTriggered);
+
+private:
+	ThumbnailSliderPrivate* const d;
+};
+
+
+} // namespace
+
+#endif /* THUMBNAILSLIDER_H */
