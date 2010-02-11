0e709b18fdf763f303f9acd9821dcabcba2b36df
Aurélien Gâteau <agateau@kde.org>
2010-02-11 22:02:20 +0000

    Fix build
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=1088911

--- lib/imageview.cpp
+++ lib/imageview.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "imageview.moc"
 
 // Qt
+#include <QApplication>
 #include <QPainter>
 #include <QPaintEvent>
 #include <QPointer>
