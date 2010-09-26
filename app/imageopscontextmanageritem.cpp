0174199f5b91bd90e9ce74f62229b8414ca186c7
Aurélien Gâteau <agateau@kde.org>
2010-09-26 21:43:28 +0000

    Moved ResizeImageOperation to resize/ dir
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=1180057

--- app/imageopscontextmanageritem.cpp
+++ app/imageopscontextmanageritem.cpp
@@ -44,7 +44,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <lib/redeyereduction/redeyereductiontool.h>
 #include <lib/gwenviewconfig.h>
 #include <lib/imageview.h>
-#include <lib/resizeimageoperation.h>
+#include <lib/resize/resizeimageoperation.h>
 #include <lib/transformimageoperation.h>
 
 namespace Gwenview {
