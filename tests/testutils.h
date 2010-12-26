5abc2994a61de2fa6c5c2d2cbf22d3614b730213
Pau Garcia i Quiles <pgquiles@elpauer.org>
2010-12-26 23:54:55 +0000

    Fix MSVC compilation
    svn path=/trunk/KDE/kdegraphics/gwenview/; revision=1209493

--- tests/testutils.h
+++ tests/testutils.h
@@ -20,6 +20,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef TESTUTILS_H
 #define TESTUTILS_H
 
+#include <math.h>
+
 // Qt
 #include <QDir>
 #include <QImage>
