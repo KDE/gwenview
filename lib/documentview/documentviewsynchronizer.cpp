b954d900be3a6eee4ce422ed01d37b43d9251ef4
Aurélien Gâteau <agateau@kde.org>
2011-03-20 22:51:50 +0100

    Spread current view zoom settings when turning on sync mode

--- lib/documentview/documentviewsynchronizer.cpp
+++ lib/documentview/documentviewsynchronizer.cpp
@@ -63,6 +63,8 @@ struct DocumentViewSynchronizerPrivate {
 			if (view == mCurrentView) {
 				continue;
 			}
+			view->setZoom(mCurrentView->zoom());
+			view->setZoomToFit(mCurrentView->zoomToFit());
 		}
 	}
 
