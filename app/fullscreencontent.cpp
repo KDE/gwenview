65f1d8c62a3c6507a4ec94625c30b36e29ccdcac
Aurélien Gâteau <agateau@kde.org>
2012-04-27 09:26:47 +0200

    Unused include--

--- app/fullscreencontent.cpp
+++ app/fullscreencontent.cpp
@@ -30,9 +30,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <QLabel>
 #include <QMenu>
 #include <QPointer>
-#include <QPushButton>
-#include <QStyle>
-#include <QToolButton>
 #include <QWidgetAction>
 
 // KDE
