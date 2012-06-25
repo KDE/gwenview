b2af5e0e890f8b6aab2273d06f2ab6a0af468f91
Aurélien Gâteau <agateau@kde.org>
2012-06-25 15:45:24 +0200

    Do not return 0 in getFreeMemory for the first two seconds
    Copied from okular repo: bf47b97cc28238599175477fe61acc2625f595e4

--- lib/memoryutils.cpp
+++ lib/memoryutils.cpp
@@ -83,7 +83,7 @@ qulonglong getTotalMemory()
 
 qulonglong getFreeMemory()
 {
-    static QTime lastUpdate = QTime::currentTime();
+    static QTime lastUpdate = QTime::currentTime().addSecs(-3);
     static qulonglong cachedValue = 0;
 
     if ( qAbs( lastUpdate.secsTo( QTime::currentTime() ) ) <= 2 )
