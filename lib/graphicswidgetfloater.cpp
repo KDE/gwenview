KDE/gwenview

commit a895c45f97cab6824ca8b02da6161723b1b01c23

Fix video hud sometimes showing half hidden
BUG: 302773

lib/graphicswidgetfloater.cpp
@@ -65,6 +65,10 @@ struct GraphicsWidgetFloaterPrivate
         parentWidth = mParent->size().width();
         parentHeight = mParent->size().height();
 
+        if (parentWidth == 0 || parentHeight == 0) {
+            return;
+        }
+
         if (mAlignment & Qt::AlignLeft) {
             posX = mHorizontalMargin;
         } else if (mAlignment & Qt::AlignHCenter) {
@@ -72,9 +76,6 @@ struct GraphicsWidgetFloaterPrivate
         } else if (mAlignment & Qt::AlignJustify) {
             posX = mHorizontalMargin;
             childWidth = parentWidth - 2 * mHorizontalMargin;
-            QRectF childGeometry = mChild->geometry();
-            childGeometry.setWidth(childWidth);
-            mChild->setGeometry(childGeometry);
         } else {
             posX = parentWidth - childWidth - mHorizontalMargin;
         }

commit d48ee33a57efc6d9a0d58e13830b5fae278c04ad

Bring back compare huds to the bottom center of the view
Commit a895c45f97cab6824ca8b02da6161723b1b01c23 broke the positioning of
compare huds by returning too early.

REVIEW: 108638
FIXED-IN: 4.10.0

lib/graphicswidgetfloater.cpp
@@ -53,7 +53,6 @@ struct GraphicsWidgetFloaterPrivate
         if (mInsideUpdateChildGeometry) {
             return;
         }
-        mInsideUpdateChildGeometry = true;
 
         int posX, posY;
         int childWidth, childHeight;
@@ -88,8 +87,8 @@ struct GraphicsWidgetFloaterPrivate
             posY = parentHeight - childHeight - mVerticalMargin;
         }
 
+        mInsideUpdateChildGeometry = true;
         mChild->setGeometry(posX, posY, childWidth, childHeight);
-
         mInsideUpdateChildGeometry = false;
     }
 };