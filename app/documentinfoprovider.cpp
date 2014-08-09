c0c0e068d9d047403057ae7c215cc9d00f64c224
David Edmundson <kde@davidedmundson.co.uk>
2014-08-09 23:25:29 +0200

    More include fixing

--- app/documentinfoprovider.cpp
+++ app/documentinfoprovider.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "documentinfoprovider.moc"
+#include "documentinfoprovider.h"
 
 // Qt
 #include <QPixmap>
--- lib/document/document.cpp
+++ lib/document/document.cpp
@@ -17,8 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
-#include "document.moc"
-// #include "document_p.moc"
+#include "document.h"
+#include "document_p.h"
 
 // Qt
 #include <QApplication>
