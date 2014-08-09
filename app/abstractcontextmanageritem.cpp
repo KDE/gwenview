ff93a52f42f0115a639f9cff8a8be93373f111fd
David Edmundson <kde@davidedmundson.co.uk>
2014-08-09 22:57:40 +0200

    Fix double moc problems.

--- app/abstractcontextmanageritem.cpp
+++ app/abstractcontextmanageritem.cpp
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "abstractcontextmanageritem.moc"
 
+#include "abstractcontextmanageritem.h"
+
 // Local
 #include <lib/contextmanager.h>
 
--- lib/document/document.cpp
+++ lib/document/document.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 #include "document.moc"
-#include "document_p.moc"
+// #include "document_p.moc"
 
 // Qt
 #include <QApplication>
