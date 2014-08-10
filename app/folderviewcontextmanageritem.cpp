47577bfb2b03a6f2a084e098fc02d5e7f3a77cd2
David Edmundson <kde@davidedmundson.co.uk>
2014-08-10 15:02:17 +0200

    add include

--- app/folderviewcontextmanageritem.cpp
+++ app/folderviewcontextmanageritem.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <QDragEnterEvent>
 #include <QHeaderView>
 #include <QTreeView>
+#include <QMimeData>
 
 // KDE
 #include <KDebug>
