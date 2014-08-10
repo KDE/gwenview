2b87c2780cfcae876ede23a7fd6edc1b3f2acc47
David Edmundson <kde@davidedmundson.co.uk>
2014-08-10 21:39:11 +0200

    KDateTime -> QDateTime fix

--- lib/thumbnailview/thumbnailview.cpp
+++ lib/thumbnailview/thumbnailview.cpp
@@ -218,7 +218,7 @@ struct ThumbnailViewPrivate
         QPixmap pix;
         QSize fullSize;
         mDocumentInfoProvider->thumbnailForDocument(url, group, &pix, &fullSize);
-        mThumbnailForUrl[url] = Thumbnail(QPersistentModelIndex(index), QDateTime::currentLocalDateTime());
+        mThumbnailForUrl[url] = Thumbnail(QPersistentModelIndex(index), QDateTime::currentDateTime());
         q->setThumbnail(item, pix, fullSize, 0);
     }
 
