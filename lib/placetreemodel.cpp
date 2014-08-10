08b30701edbcf5a77013a2a2763c20994cecf940
David Edmundson <kde@davidedmundson.co.uk>
2014-08-10 17:05:48 +0200

    More includes

--- lib/placetreemodel.cpp
+++ lib/placetreemodel.cpp
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <QDebug>
 #include <KDirLister>
 #include <KFilePlacesModel>
+#include <KUrl>
 
 // Local
 #include <lib/semanticinfo/sorteddirmodel.h>
