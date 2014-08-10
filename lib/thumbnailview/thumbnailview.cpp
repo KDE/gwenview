36b034594a612aab9a97131c7bbfcedd3ceb3048
David Edmundson <kde@davidedmundson.co.uk>
2014-08-10 19:59:25 +0200

    fix headers

--- lib/thumbnailview/thumbnailview.cpp
+++ lib/thumbnailview/thumbnailview.cpp
@@ -34,6 +34,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <QTimer>
 #include <QToolTip>
 #include <QDrag>
+#include <QMimeData>
 
 // KDE
 #include <KDebug>
