2fc7dd245ef98f48f79a1b063a87e23f82843cdd
Montel Laurent <montel@kde.org>
2014-10-19 12:23:52 +0200

    Comment includes too

--- lib/semanticinfo/baloosemanticinfobackend.cpp
+++ lib/semanticinfo/baloosemanticinfobackend.cpp
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 // Baloo
 #include <baloo/file.h>
-#include <baloo/filefetchjob.h>
-#include <baloo/filemodifyjob.h>
+//#include <baloo/filefetchjob.h>
+//#include <baloo/filemodifyjob.h>
 #include <baloo/taglistjob.h>
 
 namespace Gwenview
