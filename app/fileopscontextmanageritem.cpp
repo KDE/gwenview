48263adc7aa2d4ee5c3a03963ff5da745e131098
David Faure <faure@kde.org>
2014-10-21 20:08:57 +0200

    Use pasteActionText from KIO

--- app/fileopscontextmanageritem.cpp
+++ app/fileopscontextmanageritem.cpp
@@ -86,46 +86,6 @@ struct FileOpsContextManagerItemPrivate
     KNewFileMenu * mNewFileMenu;
     bool mInTrash;
 
-    QPair<bool, QString> pasteInfo(const QUrl& targetUrl)
-    {
-        QPair<bool, QString> ret;
-        QClipboard* clipboard = QApplication::clipboard();
-        const QMimeData* mimeData = clipboard->mimeData();
-
-        bool canPasteData = false;
-        QList<QUrl> urls;
-
-        // mimeData can be 0 according to https://bugs.kde.org/show_bug.cgi?id=335053
-        if (mimeData) {
-            canPasteData = KIO::canPasteMimeData(mimeData);
-            urls = KUrlMimeData::urlsFromMimeData(mimeData);
-        } else {
-            qWarning() << "QApplication::clipboard()->mimeData() is 0!";
-        }
-
-        if (!urls.isEmpty() || canPasteData) {
-            // disable the paste action if no writing is supported
-            KFileItem item(KFileItem::Unknown, KFileItem::Unknown, targetUrl);
-            ret.first = KFileItemListProperties(KFileItemList() << item).supportsWriting();
-
-            if (urls.count() == 1) {
-                const KFileItem item(KFileItem::Unknown, KFileItem::Unknown, urls.first(), true);
-                ret.second = item.isDir() ? i18nc("@action:inmenu", "Paste One Folder") :
-                                            i18nc("@action:inmenu", "Paste One File");
-
-            } else if (!urls.isEmpty()) {
-                ret.second = i18ncp("@action:inmenu", "Paste One Item", "Paste %1 Items", urls.count());
-            } else {
-                ret.second = i18nc("@action:inmenu", "Paste Clipboard Contents...");
-            }
-        } else {
-            ret.first = false;
-            ret.second = i18nc("@action:inmenu", "Paste");
-        }
-
-        return ret;
-    }
-
     QList<QUrl> urlList() const
     {
         QList<QUrl> urlList;
@@ -334,9 +294,12 @@ void FileOpsContextManagerItem::updateActions()
 
 void FileOpsContextManagerItem::updatePasteAction()
 {
-    QPair<bool, QString> info = d->pasteInfo(d->pasteTargetUrl());
-    d->mPasteAction->setEnabled(info.first);
-    d->mPasteAction->setText(info.second);
+    const QMimeData *mimeData = QApplication::clipboard()->mimeData();
+    bool enable;
+    KFileItem destItem(d->pasteTargetUrl());
+    const QString text = KIO::pasteActionText(mimeData, &enable, destItem);
+    d->mPasteAction->setEnabled(enable);
+    d->mPasteAction->setText(text);
 }
 
 void FileOpsContextManagerItem::updateSideBarContent()
