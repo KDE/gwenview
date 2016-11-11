b7e48e4aecd19e86a26469f99ef1669457c63eae
Aurélien Gâteau <agateau@kde.org>
2016-11-11 12:06:33 +0100

    Define application domain

--- importer/main.cpp
+++ importer/main.cpp
@@ -37,6 +37,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 int main(int argc, char *argv[])
 {
+    KLocalizedString::setApplicationDomain("gwenview");
     QApplication app(argc, argv);
 
     QScopedPointer<KAboutData> aboutData(
