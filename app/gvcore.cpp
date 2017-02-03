KDE/gwenview

commit bf3d936c583f259d74a79269525bcf04dd2bb6f7

port gvcore, part and mainwindow from kfiledialog to qfiledialog

app/gvcore.cpp
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <QMimeDatabase>
 
 // KDE
-#include <KFileDialog>
+#include <QFileDialog>
 #include <KColorScheme>
 #include <KIO/StatJob>
 #include <KJobWidgets>
@@ -68,19 +68,17 @@ struct GvCorePrivate
 
     bool showSaveAsDialog(const QUrl &url, QUrl* outUrl, QByteArray* format)
     {
-        KFileDialog dialog(url, QString(), mMainWindow);
-        dialog.setOperationMode(KFileDialog::Saving);
-        dialog.setSelection(url.fileName());
+        QFileDialog dialog(mMainWindow);
+        dialog.setAcceptMode(QFileDialog::AcceptSave);
+        dialog.selectUrl(url);
 
         QStringList supportedMimetypes;
         for (const QByteArray &mimeName : QImageWriter::supportedMimeTypes()) {
             supportedMimetypes.append(QString::fromLocal8Bit(mimeName));
         }
 
-        dialog.setMimeFilter(
-            supportedMimetypes,
-            MimeTypeUtils::urlMimeType(url)         // Default
-        );
+        dialog.setMimeTypeFilters(supportedMimetypes);
+        dialog.selectMimeTypeFilter(MimeTypeUtils::urlMimeType(url));
 
         // Show dialog
         do {
@@ -88,29 +86,26 @@ struct GvCorePrivate
                 return false;
             }
 
-            const QString mimeType = dialog.currentMimeFilter();
-            if (mimeType.isEmpty()) {
-                KMessageBox::sorry(
-                    mMainWindow,
-                    i18nc("@info",
-                          "No image format selected.")
-                );
-                continue;
+            QList<QUrl> files = dialog.selectedUrls();
+            if (files.isEmpty()) {
+                return false;
             }
 
-            const QStringList typeList = QMimeDatabase().mimeTypeForName(mimeType).suffixes();
+            QString filename = files.first().fileName();
+
+            const QStringList typeList = QMimeDatabase().mimeTypeForName(filename).suffixes();
             if (typeList.count() > 0) {
-                *format = typeList[0].toAscii();
+                *format = typeList.first().toLocal8Bit();
                 break;
             }
             KMessageBox::sorry(
                 mMainWindow,
                 i18nc("@info",
-                      "Gwenview cannot save images as %1.", mimeType)
+                      "Gwenview cannot save images as %1.", QFileInfo(filename).suffix())
             );
         } while (true);
 
-        *outUrl = dialog.selectedUrl();
+        *outUrl = dialog.selectedUrls().first();
         return true;
     }
 

app/mainwindow.cpp
@@ -39,7 +39,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // KDE
 #include <KActionCategory>
 #include <KActionCollection>
-#include <KFileDialog>
+#include <QFileDialog>
 #include <KFileItem>
 #include <KLocalizedString>
 #include <KMessageBox>
@@ -1327,16 +1327,19 @@ void MainWindow::openFile()
 {
     QUrl dirUrl = d->mContextManager->currentDirUrl();
 
-    KFileDialog dialog(dirUrl, QString(), this);
+    QFileDialog dialog(this);
+    dialog.selectUrl(dirUrl);
     dialog.setWindowTitle(i18nc("@title:window", "Open Image"));
     const QStringList mimeFilter = MimeTypeUtils::imageMimeTypes();
-    dialog.setMimeFilter(mimeFilter);
-    dialog.setOperationMode(KFileDialog::Opening);
+    dialog.setMimeTypeFilters(mimeFilter);
+    dialog.setAcceptMode(QFileDialog::AcceptOpen);
     if (!dialog.exec()) {
         return;
     }
 
-    openUrl(dialog.selectedUrl());
+    if (!dialog.selectedUrls().isEmpty()) {
+        openUrl(dialog.selectedUrls().first());
+    }
 }
 
 void MainWindow::openUrl(const QUrl& url)

part/gvpart.cpp
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // KDE
 #include <KAboutData>
 #include <KActionCollection>
-#include <KFileDialog>
+#include <QFileDialog>
 #include <KIconLoader>
 #include <KIO/Job>
 #include <KIO/JobUiDelegate>
@@ -149,7 +149,7 @@ void GVPart::showContextMenu()
 void GVPart::saveAs()
 {
     const QUrl srcUrl = url();
-    const QUrl dstUrl = KFileDialog::getSaveUrl(srcUrl, QString(), widget());
+    const QUrl dstUrl = QFileDialog::getSaveFileUrl(widget(), QString(), srcUrl);
     if (!dstUrl.isValid()) {
         return;
     }

commit 89e5738be3e1bad4d39cdfb006c70f3206483eb1

Fix mimetype check in SaveAs action
Regression caused by commit bf3d936c583f259d74a79269525bcf04dd2bb6f7

Differential Revision: https://phabricator.kde.org/D4404

app/gvcore.cpp
@@ -92,9 +92,9 @@ struct GvCorePrivate
 
             QString filename = files.first().fileName();
 
-            const QStringList typeList = QMimeDatabase().mimeTypeForName(filename).suffixes();
-            if (typeList.count() > 0) {
-                *format = typeList.first().toLocal8Bit();
+            const QMimeType mimeType = QMimeDatabase().mimeTypeForFile(filename, QMimeDatabase::MatchExtension);
+            if (mimeType.isValid()) {
+                *format = mimeType.preferredSuffix().toLocal8Bit();
                 break;
             }
             KMessageBox::sorry(