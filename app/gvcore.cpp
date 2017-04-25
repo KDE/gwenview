KDE/gwenview

commit 840d1c40bc906b0961418c8b738aef3971e7dce2

port to QStandardPaths

app/gvcore.cpp
@@ -34,7 +34,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <KJobWidgets>
 #include <KLocalizedString>
 #include <KMessageBox>
-#include <KStandardDirs>
 
 // Local
 #include <lib/binder.h>
@@ -128,19 +127,19 @@ struct GvCorePrivate
         QString name = GwenviewConfig::fullScreenColorScheme();
         if (name.isEmpty()) {
             // Default color scheme
-            QString path = KStandardDirs::locate("data", "gwenview/color-schemes/fullscreen.colors");
+            QString path = QStandardPaths::locate(QStandardPaths::AppDataLocation, "color-schemes/fullscreen.colors");
             config = KSharedConfig::openConfig(path);
         } else if (name.contains('/')) {
             // Full path to a .colors file
             config = KSharedConfig::openConfig(name);
         } else {
             // Standard KDE color scheme
-            config = KSharedConfig::openConfig(QString("color-schemes/%1.colors").arg(name), KConfig::FullConfig, QStandardPaths::DataLocation);
+            config = KSharedConfig::openConfig(QString("color-schemes/%1.colors").arg(name), KConfig::FullConfig, QStandardPaths::AppDataLocation);
         }
         mPalettes[GvCore::FullScreenPalette] = KColorScheme::createApplicationPalette(config);
 
         pal = mPalettes[GvCore::FullScreenPalette];
-        QString path = KStandardDirs::locate("data", "gwenview/images/background.png");
+        QString path = QStandardPaths::locate(QStandardPaths::AppDataLocation, "images/background.png");
         QPixmap bgTexture(path);
         pal.setBrush(QPalette::Base, bgTexture);
         mPalettes[GvCore::FullScreenViewPalette] = pal;
@@ -171,7 +170,7 @@ GvCore::~GvCore()
 QAbstractItemModel* GvCore::recentFoldersModel() const
 {
     if (!d->mRecentFoldersModel) {
-        d->mRecentFoldersModel = new HistoryModel(const_cast<GvCore*>(this), KStandardDirs::locateLocal("appdata", "recentfolders/"));
+        d->mRecentFoldersModel = new HistoryModel(const_cast<GvCore*>(this), QStandardPaths::locate(QStandardPaths::AppLocalDataLocation, "recentfolders/"));
     }
     return d->mRecentFoldersModel;
 }

tests/auto/placetreemodeltest.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // KDE
 #include <QDebug>
-#include <KStandardDirs>
+#include <QStandardPaths>
 #include <QStandardPaths>
 #include <qtest.h>
 

tests/auto/testutils.cpp
@@ -29,7 +29,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <KIO/MkdirJob>
 #include <KIO/FileCopyJob>
 #include <KJobWidgets>
-#include <KStandardDirs>
 #include <QStandardPaths>
 
 QUrl setUpRemoteTestDir(const QString& testFile)

commit 5975b74974d06a1b8ac584edaa9f6b8ed9fa432d

History (recentfolders) not working / folder never created
Summary:
Folder History is not working anymore.

BUG: 379177

Commit 840d1c40bc906b0961418c8b738aef3971e7dce2 broke it.

> QStandardPaths::locate(QStandardPaths::AppLocalDataLocation, "recentfolders/")

always returns an empty string "" when either the recentfolders folder or the ~/.local/share/gwenview folder don't exist.
See: http://doc.qt.io/qt-5/qstandardpaths.html#locate
> If no such file or directory can be found, an empty string is returned

KStandardDirs::locateLocal behaves differently (from the api doc)
> No check is made if the specified filename actually exists. Missing directories are created.

As we don't want to switch back to KStandardDirs I came up with a working patch using QStandardPaths::writableLocation (see attachment) as
> Note: The storage location returned can be a directory that does not exist; i.e., it may need to be created by the system or the user.

(from : http://doc.qt.io/qt-5/qstandardpaths.html#writableLocation )

Reviewers: sandsmark, ltoscano

Reviewed By: ltoscano

Subscribers: ltoscano, cfeck

Differential Revision: https://phabricator.kde.org/D5572

app/gvcore.cpp
@@ -170,7 +170,7 @@ GvCore::~GvCore()
 QAbstractItemModel* GvCore::recentFoldersModel() const
 {
     if (!d->mRecentFoldersModel) {
-        d->mRecentFoldersModel = new HistoryModel(const_cast<GvCore*>(this), QStandardPaths::locate(QStandardPaths::AppLocalDataLocation, "recentfolders/"));
+        d->mRecentFoldersModel = new HistoryModel(const_cast<GvCore*>(this), QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/recentfolders/");
     }
     return d->mRecentFoldersModel;
 }