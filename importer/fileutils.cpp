KDE/gwenview

commit 017b4fe5dc7f4b6e876cfd7b108dcebbf609ae94

Start bringing back Gwenview Importer
Port from KUrl to QUrl, and KDateTime to QDateTime

importer/fileutils.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 // Qt
 #include <QFile>
 #include <QFileInfo>
+#include <QUrl>
 
 // KDE
 #include <QDebug>
@@ -33,7 +34,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <KIO/JobClasses>
 #include <KIO/JobUiDelegate>
 #include <KIO/NetAccess>
-#include <KUrl>
 
 // libc
 #include <errno.h>
@@ -45,7 +45,7 @@ namespace Gwenview
 namespace FileUtils
 {
 
-bool contentsAreIdentical(const KUrl& url1, const KUrl& url2, QWidget* authWindow)
+bool contentsAreIdentical(const QUrl& url1, const QUrl& url2, QWidget* authWindow)
 {
     // FIXME: Support remote urls
     QFile file1(KIO::NetAccess::mostLocalUrl(url1, authWindow).toLocalFile());
@@ -79,9 +79,9 @@ bool contentsAreIdentical(const KUrl& url1, const KUrl& url2, QWidget* authWindo
     }
 }
 
-RenameResult rename(const KUrl& src, const KUrl& dst_, QWidget* authWindow)
+RenameResult rename(const QUrl& src, const QUrl& dst_, QWidget* authWindow)
 {
-    KUrl dst = dst_;
+    QUrl dst = dst_;
     RenameResult result = RenamedOK;
     int count = 1;
 
@@ -104,15 +104,18 @@ RenameResult rename(const KUrl& src, const KUrl& dst_, QWidget* authWindow)
         if (srcSize == dstSize && contentsAreIdentical(src, dst, authWindow)) {
             // Already imported, skip it
             KIO::Job* job = KIO::file_delete(src, KIO::HideProgressInfo);
+            // FIXME KF5
+            /*
             if (job->ui()) {
                 job->ui()->setWindow(authWindow);
             }
+            */
 
             return Skipped;
         }
         result = RenamedUnderNewName;
 
-        dst.setFileName(prefix + QString::number(count) + suffix);
+        dst.setPath(dst.path() + '/' + prefix + QString::number(count) + suffix);
         ++count;
     }
 

importer/importdialog.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "importdialog.moc"
+#include "importdialog.h"
 
 // Qt
 #include <QApplication>
@@ -62,8 +62,8 @@ public:
 
     void deleteImportedUrls()
     {
-        KUrl::List importedUrls = mImporter->importedUrlList();
-        KUrl::List skippedUrls = mImporter->skippedUrlList();
+        QList<QUrl> importedUrls = mImporter->importedUrlList();
+        QList<QUrl> skippedUrls = mImporter->skippedUrlList();
         int importedCount = importedUrls.count();
         int skippedCount = skippedUrls.count();
 
@@ -120,7 +120,7 @@ public:
         if (answer != KMessageBox::Yes) {
             return;
         }
-        KUrl::List urls = importedUrls + skippedUrls;
+        QList<QUrl> urls = importedUrls + skippedUrls;
         while (true) {
             KIO::Job* job = KIO::del(urls);
             if (KIO::NetAccess::synchronousRun(job, q)) {
@@ -144,11 +144,11 @@ public:
 
     void startGwenview()
     {
-        KService::Ptr service = KService::serviceByDesktopName("gwenview");
+        KService::Ptr service = KService::serviceByDesktopName("org.kde.gwenview");
         if (!service) {
             qCritical() << "Could not find gwenview";
         } else {
-            KRun::run(*service, KUrl::List() << mThumbnailPage->destinationUrl(), 0 /* window */);
+            KRun::run(*service, {mThumbnailPage->destinationUrl()}, 0 /* window */);
         }
     }
 
@@ -182,11 +182,11 @@ ImportDialog::ImportDialog()
             SLOT(showImportError(QString)));
     d->mThumbnailPage = new ThumbnailPage;
 
-    KUrl url = ImporterConfig::destinationUrl();
+    QUrl url = ImporterConfig::destinationUrl();
     if (!url.isValid()) {
         url = QUrl::fromLocalFile(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation));
         int year = QDate::currentDate().year();
-        url.addPath(QString::number(year));
+        url.setPath(url.path() + '/' + QString::number(year));
     }
     d->mThumbnailPage->setDestinationUrl(url);
 
@@ -209,7 +209,7 @@ ImportDialog::ImportDialog()
 
     d->mCentralWidget->setCurrentWidget(d->mThumbnailPage);
 
-    setWindowIcon(KIcon("gwenview"));
+    setWindowIcon(QIcon::fromTheme("gwenview"));
     setAutoSaveSettings();
 }
 
@@ -223,12 +223,12 @@ QSize ImportDialog::sizeHint() const
     return QSize(700, 500);
 }
 
-void ImportDialog::setSourceUrl(const KUrl& url, const QString& deviceUdi)
+void ImportDialog::setSourceUrl(const QUrl& url, const QString& deviceUdi)
 {
     QString name, iconName;
     if (deviceUdi.isEmpty()) {
-        name = url.pathOrUrl();
-        iconName = KProtocolInfo::icon(url.protocol());
+        name = url.url(QUrl::PreferLocalFile);
+        iconName = KProtocolInfo::icon(url.scheme());
         if (iconName.isEmpty()) {
             iconName = "folder";
         }
@@ -242,7 +242,7 @@ void ImportDialog::setSourceUrl(const KUrl& url, const QString& deviceUdi)
 
 void ImportDialog::startImport()
 {
-    KUrl url = d->mThumbnailPage->destinationUrl();
+    QUrl url = d->mThumbnailPage->destinationUrl();
     ImporterConfig::setDestinationUrl(url);
     ImporterConfig::self()->save();
 

importer/importer.cpp
@@ -19,16 +19,16 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "importer.moc"
+#include "importer.h"
 
 // Qt
+#include <QDateTime>
+#include <QDebug>
+#include <QUrl>
 
 // KDE
-#include <KDateTime>
-#include <QDebug>
 #include <KFileItem>
 #include <KLocale>
-#include <KUrl>
 #include <KIO/CopyJob>
 #include <KIO/DeleteJob>
 #include <KIO/Job>
@@ -53,26 +53,26 @@ struct ImporterPrivate
     Importer* q;
     QWidget* mAuthWindow;
     std::auto_ptr<FileNameFormater> mFileNameFormater;
-    KUrl mTempImportDir;
+    QUrl mTempImportDirUrl;
 
     /* @defgroup reset Should be reset in start()
      * @{ */
-    KUrl::List mUrlList;
-    KUrl::List mImportedUrlList;
-    KUrl::List mSkippedUrlList;
+    QList<QUrl> mUrlList;
+    QList<QUrl> mImportedUrlList;
+    QList<QUrl> mSkippedUrlList;
     int mRenamedCount;
     int mProgress;
     int mJobProgress;
     /* @} */
 
-    KUrl mCurrentUrl;
+    QUrl mCurrentUrl;
 
     void emitError(const QString& message)
     {
         QMetaObject::invokeMethod(q, "error", Q_ARG(QString, message));
     }
 
-    bool createImportDir(const KUrl url)
+    bool createImportDir(const QUrl url)
     {
         Q_ASSERT(url.isLocalFile());
         // FIXME: Support remote urls
@@ -82,8 +82,9 @@ struct ImporterPrivate
             return false;
         }
         QString message;
-        mTempImportDir = FileUtils::createTempDir(url.toLocalFile(), ".gwenview_importer-", &message);
-        if (mTempImportDir.isEmpty()) {
+        QString dir = FileUtils::createTempDir(url.toLocalFile(), ".gwenview_importer-", &message);
+        mTempImportDirUrl = QUrl::fromLocalFile(dir);
+        if (mTempImportDirUrl.isEmpty()) {
             emitError(i18n("Could not create temporary upload folder:\n%1", message));
             return false;
         }
@@ -97,22 +98,23 @@ struct ImporterPrivate
             return;
         }
         mCurrentUrl = mUrlList.takeFirst();
-        KUrl dst = mTempImportDir;
-        dst.addPath(mCurrentUrl.fileName());
+        QUrl dst = mTempImportDirUrl;
+        dst.setPath(dst.path() + '/' + mCurrentUrl.fileName());
         KIO::Job* job = KIO::copy(mCurrentUrl, dst, KIO::HideProgressInfo);
+        /* KF5 FIXME
         if (job->ui()) {
             job->ui()->setWindow(mAuthWindow);
         }
+        */
         QObject::connect(job, SIGNAL(result(KJob*)),
                          q, SLOT(slotCopyDone(KJob*)));
         QObject::connect(job, SIGNAL(percent(KJob*,ulong)),
                          q, SLOT(slotPercent(KJob*,ulong)));
     }
 
-    void renameImportedUrl(const KUrl& src)
+    void renameImportedUrl(const QUrl& src)
     {
-        KUrl dst = src;
-        dst.cd("..");
+        QUrl dst = src.resolved(QUrl(".."));
         QString fileName;
         if (mFileNameFormater.get()) {
             KFileItem item(KFileItem::Unknown, KFileItem::Unknown, src, true /* delayedMimeTypes */);
@@ -120,12 +122,12 @@ struct ImporterPrivate
             // 'src' url is temporary: if we import "foo/image.jpg" and
             // "bar/image.jpg", both images will be temporarily saved in the
             // 'src' url.
-            KDateTime dateTime = TimeUtils::dateTimeForFileItem(item, TimeUtils::SkipCache);
+            QDateTime dateTime = TimeUtils::dateTimeForFileItem(item, TimeUtils::SkipCache);
             fileName = mFileNameFormater->format(src, dateTime);
         } else {
             fileName = src.fileName();
         }
-        dst.setFileName(fileName);
+        dst.setPath(dst.path() + '/' + fileName);
 
         FileUtils::RenameResult result = FileUtils::rename(src, dst, mAuthWindow);
         switch (result) {
@@ -169,7 +171,7 @@ void Importer::setAutoRenameFormat(const QString& format)
     }
 }
 
-void Importer::start(const KUrl::List& list, const KUrl& destination)
+void Importer::start(const QList<QUrl>& list, const QUrl& destination)
 {
     d->mUrlList = list;
     d->mImportedUrlList.clear();
@@ -191,7 +193,7 @@ void Importer::start(const KUrl::List& list, const KUrl& destination)
 void Importer::slotCopyDone(KJob* _job)
 {
     KIO::CopyJob* job = static_cast<KIO::CopyJob*>(_job);
-    KUrl url = job->destUrl();
+    QUrl url = job->destUrl();
     if (job->error()) {
         qWarning() << "FIXME: What do we do with failed urls?";
         advance();
@@ -204,10 +206,12 @@ void Importer::slotCopyDone(KJob* _job)
 
 void Importer::finalizeImport()
 {
-    KIO::Job* job = KIO::del(d->mTempImportDir, KIO::HideProgressInfo);
+    KIO::Job* job = KIO::del(d->mTempImportDirUrl, KIO::HideProgressInfo);
+    /* KF5 FIXME
     if (job->ui()) {
         job->ui()->setWindow(d->mAuthWindow);
     }
+    */
     importFinished();
 }
 
@@ -229,12 +233,12 @@ void Importer::emitProgressChanged()
     progressChanged(d->mProgress * 100 + d->mJobProgress);
 }
 
-KUrl::List Importer::importedUrlList() const
+QList<QUrl> Importer::importedUrlList() const
 {
     return d->mImportedUrlList;
 }
 
-KUrl::List Importer::skippedUrlList() const
+QList<QUrl> Importer::skippedUrlList() const
 {
     return d->mSkippedUrlList;
 }

CMakeLists.txt
@@ -59,6 +59,7 @@ find_package(KF5KIO 5.4 REQUIRED)
 find_package(KF5 REQUIRED COMPONENTS
     KDELibs4Support
     Activities
+    ItemModels
 )
 
 ## Dependencies
@@ -111,7 +112,7 @@ add_definitions(-DTRANSLATION_DOMAIN="gwenview")
 ## dirs to build
 add_subdirectory(lib)
 add_subdirectory(app)
-# add_subdirectory(importer)
+add_subdirectory(importer)
 add_subdirectory(part)
 add_subdirectory(tests)
 add_subdirectory(icons)

importer/CMakeLists.txt
@@ -27,24 +27,28 @@ ki18n_wrap_ui(importer_SRCS
     thumbnailpage.ui
     )
 
-file(GLOB ICONS_SRCS "${CMAKE_CURRENT_SOURCE_DIR}/../icons/hi*-apps-gwenview-importer.png")
+file(GLOB ICONS_SRCS "${CMAKE_CURRENT_SOURCE_DIR}/../icons/*-apps-gwenview.png")
 ecm_add_app_icon(importer_SRCS ICONS ${ICONS_SRCS})
 
 kconfig_add_kcfg_files(importer_SRCS
     importerconfig.kcfgc
     )
 
+add_definitions(-DQT_NO_URL_CAST_FROM_STRING)
+
 add_executable(gwenview_importer ${importer_SRCS})
 
 target_link_libraries(gwenview_importer
     gwenviewlib
     KF5::KIOCore
+    KF5::ItemModels
     Qt5::Core
     )
 
 target_link_libraries(gwenview_importer
     LINK_INTERFACE_LIBRARIES
     KF5::KIOCore
+    KF5::ItemModels
     Qt5::Core
     )
 

importer/dialogpage.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "dialogpage.moc"
+#include "dialogpage.h"
 
 // Qt
 #include <QEventLoop>

importer/documentdirfinder.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 */
 // Self
-#include "documentdirfinder.moc"
+#include "documentdirfinder.h"
 
 // Qt
 
@@ -35,19 +35,19 @@ namespace Gwenview
 
 struct DocumentDirFinderPrivate
 {
-    KUrl mRootUrl;
+    QUrl mRootUrl;
     KDirLister* mDirLister;
 
-    KUrl mFoundDirUrl;
+    QUrl mFoundDirUrl;
 };
 
-DocumentDirFinder::DocumentDirFinder(const KUrl& rootUrl)
+DocumentDirFinder::DocumentDirFinder(const QUrl& rootUrl)
 : d(new DocumentDirFinderPrivate)
 {
     d->mRootUrl = rootUrl;
     d->mDirLister = new KDirLister(this);
-    connect(d->mDirLister, SIGNAL(itemsAdded(KUrl,KFileItemList)),
-            SLOT(slotItemsAdded(KUrl,KFileItemList)));
+    connect(d->mDirLister, SIGNAL(itemsAdded(QUrl,KFileItemList)),
+            SLOT(slotItemsAdded(QUrl,KFileItemList)));
     connect(d->mDirLister, SIGNAL(completed()),
             SLOT(slotCompleted()));
     d->mDirLister->openUrl(rootUrl);
@@ -63,7 +63,7 @@ void DocumentDirFinder::start()
     d->mDirLister->openUrl(d->mRootUrl);
 }
 
-void DocumentDirFinder::slotItemsAdded(const KUrl& dir, const KFileItemList& list)
+void DocumentDirFinder::slotItemsAdded(const QUrl& dir, const KFileItemList& list)
 {
     Q_FOREACH(const KFileItem & item, list) {
         MimeTypeUtils::Kind kind = MimeTypeUtils::fileItemKind(item);
@@ -96,15 +96,15 @@ void DocumentDirFinder::slotItemsAdded(const KUrl& dir, const KFileItemList& lis
 void DocumentDirFinder::slotCompleted()
 {
     if (d->mFoundDirUrl.isValid()) {
-        KUrl url = d->mFoundDirUrl;
-        d->mFoundDirUrl = KUrl();
+        QUrl url = d->mFoundDirUrl;
+        d->mFoundDirUrl = QUrl();
         d->mDirLister->openUrl(url);
     } else {
         finish(d->mRootUrl, NoDocumentFound);
     }
 }
 
-void DocumentDirFinder::finish(const KUrl& url, DocumentDirFinder::Status status)
+void DocumentDirFinder::finish(const QUrl& url, DocumentDirFinder::Status status)
 {
     disconnect(d->mDirLister, 0, this, 0);
     emit done(url, status);

importer/documentdirfinder.h
@@ -55,21 +55,21 @@ public:
         MultipleDirsFound
     };
 
-    DocumentDirFinder(const KUrl& rootUrl);
+    DocumentDirFinder(const QUrl& rootUrl);
     ~DocumentDirFinder();
 
     void start();
 
 Q_SIGNALS:
-    void done(const KUrl&, DocumentDirFinder::Status);
+    void done(const QUrl&, DocumentDirFinder::Status);
 
 private Q_SLOTS:
-    void slotItemsAdded(const KUrl&, const KFileItemList&);
+    void slotItemsAdded(const QUrl&, const KFileItemList&);
     void slotCompleted();
 
 private:
     DocumentDirFinderPrivate* const d;
-    void finish(const KUrl&, Status);
+    void finish(const QUrl&, Status);
 };
 
 } // namespace

commit 6ce5d2f8d82f83c5a3d726ee5807ebaf7a6e3c6c

Avoid data loss when importing pictures
Summary:
Fix porting regressions, which left users of Gwenview Importer with:
- failed import (import destination still empty)
- additionally (when choosing "Delete" instead of "Keep" after import):
  pictures also removed from import source, with no way to recover

Correct additional problems remaining after fixing the import failure:
- hang on duplicate filenames
- identically named files with different content are never imported
- error dialog when deleting pictures from import source

BUG: 379615

In detail:

1st problem (introduced in 017b4fe5dc7f4b6e876cfd7b108dcebbf609ae94):

  Initially, pictures are copied to a temporary folder
  (e.g. "foo/.gwenview_importer-IcQqvo/"), before being moved/renamed
  to the final destination (e.g. "foo/"), which is determined by
  calling "cd .." on the temporary folder.

  However, mistakenly this path contains a superfluous '/'
  (e.g. "foo/.gwenview_importer-IcQqvo//"), resulting in the final
  destination reading "foo/.gwenview_importer-IcQqvo/" instead of
  "foo/". On cleanup, the temporary folder is removed, simultaneously
  deleting all new pictures.

  Fix: Omit '/' where appropriate.

2nd problem (broken by a3262e9b197ee97323ef8bf3a9dca1e13f61a74c):

  When trying to find a unique filename, the while loop "stat"ing the
  file repeats forever.

  Fix: Call "KIO::stat" and "KJobWidgets::setWindow"
       also inside the loop.

3rd problem (introduced in 017b4fe5dc7f4b6e876cfd7b108dcebbf609ae94):

  If the destination directory already contains an identically named
  file, we try to find a different name by appending a number. For
  this, we need to strip the old filename from the full path.
  Unfortunately, only calling "path()" is incorrect, giving
  "foo/pict0001.jpg/pict0001_1.jpg", rather than "foo/pict0001_1.jpg".

  Fix: Use "adjusted(QUrl::RemoveFilename)".

4th problem (broken by a3262e9b197ee97323ef8bf3a9dca1e13f61a74c):

importer/fileutils.cpp
@@ -128,7 +128,10 @@ RenameResult rename(const QUrl& src, const QUrl& dst_, QWidget* authWindow)
         }
         result = RenamedUnderNewName;
 
-        dst.setPath(dst.path() + '/' + prefix + QString::number(count) + suffix);
+        dst.setPath(dst.adjusted(QUrl::RemoveFilename).path() + prefix + QString::number(count) + suffix);
+        statJob = KIO::stat(dst);
+        KJobWidgets::setWindow(statJob, authWindow);
+
         ++count;
     }
 

importer/importdialog.cpp
@@ -121,7 +121,7 @@ public:
         QList<QUrl> urls = importedUrls + skippedUrls;
         while (true) {
             KIO::Job* job = KIO::del(urls);
-            if (!job->exec()) {
+            if (job->exec()) {
                 break;
             }
             // Deleting failed

importer/importer.cpp
@@ -98,7 +98,7 @@ struct ImporterPrivate
         }
         mCurrentUrl = mUrlList.takeFirst();
         QUrl dst = mTempImportDirUrl;
-        dst.setPath(dst.path() + '/' + mCurrentUrl.fileName());
+        dst.setPath(dst.path() + mCurrentUrl.fileName());
         KIO::Job* job = KIO::copy(mCurrentUrl, dst, KIO::HideProgressInfo);
         KJobWidgets::setWindow(job, mAuthWindow);
         QObject::connect(job, SIGNAL(result(KJob*)),
@@ -122,7 +122,7 @@ struct ImporterPrivate
         } else {
             fileName = src.fileName();
         }
-        dst.setPath(dst.path() + '/' + fileName);
+        dst.setPath(dst.path() + fileName);
 
         FileUtils::RenameResult result = FileUtils::rename(src, dst, mAuthWindow);
         switch (result) {