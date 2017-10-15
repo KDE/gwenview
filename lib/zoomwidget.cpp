KDE/gwenview

commit 3e10699ac37c4b8ffc510546913e26fb9f89aa6c

Add Fit to width
Signed-off-by: gagiD <dino.gaguliceldin@gmail.com>

lib/zoomwidget.cpp
@@ -60,10 +60,12 @@ struct ZoomWidgetPrivate
 
     StatusBarToolButton* mZoomToFitButton;
     StatusBarToolButton* mActualSizeButton;
+    StatusBarToolButton* mZoomToFitWidthButton;
     QLabel* mZoomLabel;
     ZoomSlider* mZoomSlider;
     QAction* mZoomToFitAction;
     QAction* mActualSizeAction;
+    QAction* mZoomToFitWidthAction;
 
     bool mZoomUpdatedBySlider;
 
@@ -90,12 +92,19 @@ ZoomWidget::ZoomWidget(QWidget* parent)
 
     d->mZoomToFitButton = new StatusBarToolButton;
     d->mActualSizeButton = new StatusBarToolButton;
+    d->mZoomToFitWidthButton = new StatusBarToolButton;
+    d->mZoomToFitButton->setCheckable(true);
+    d->mActualSizeButton->setCheckable(true);
+    d->mZoomToFitWidthButton->setCheckable(true);
+    d->mZoomToFitButton->setChecked(true);
 
     if (QApplication::isLeftToRight()) {
         d->mZoomToFitButton->setGroupPosition(StatusBarToolButton::GroupLeft);
+        d->mZoomToFitWidthButton->setGroupPosition(StatusBarToolButton::GroupCenter);
         d->mActualSizeButton->setGroupPosition(StatusBarToolButton::GroupRight);
     } else {
         d->mActualSizeButton->setGroupPosition(StatusBarToolButton::GroupLeft);
+        d->mZoomToFitWidthButton->setGroupPosition(StatusBarToolButton::GroupCenter);
         d->mZoomToFitButton->setGroupPosition(StatusBarToolButton::GroupRight);
     }
 
@@ -114,6 +123,7 @@ ZoomWidget::ZoomWidget(QWidget* parent)
     layout->setMargin(0);
     layout->setSpacing(0);
     layout->addWidget(d->mZoomToFitButton);
+    layout->addWidget(d->mZoomToFitWidthButton);
     layout->addWidget(d->mActualSizeButton);
     layout->addWidget(d->mZoomSlider);
     layout->addWidget(d->mZoomLabel);
@@ -124,21 +134,30 @@ ZoomWidget::~ZoomWidget()
     delete d;
 }
 
-void ZoomWidget::setActions(QAction* zoomToFitAction, QAction* actualSizeAction, QAction* zoomInAction, QAction* zoomOutAction)
+void ZoomWidget::setActions(QAction* zoomToFitAction, QAction* actualSizeAction, QAction* zoomInAction, QAction* zoomOutAction, QAction* zoomToFitWidthAction)
 {
     d->mZoomToFitAction = zoomToFitAction;
     d->mActualSizeAction = actualSizeAction;
+    d->mZoomToFitWidthAction = zoomToFitWidthAction;
 
     d->mZoomToFitButton->setDefaultAction(zoomToFitAction);
     d->mActualSizeButton->setDefaultAction(actualSizeAction);
+    d->mZoomToFitWidthButton->setDefaultAction(zoomToFitWidthAction);
 
     d->mZoomSlider->setZoomInAction(zoomInAction);
     d->mZoomSlider->setZoomOutAction(zoomOutAction);
+
+    QActionGroup *actionGroup = new QActionGroup(d->q);
+    actionGroup->addAction(d->mZoomToFitAction);
+    actionGroup->addAction(d->mZoomToFitWidthAction);
+    actionGroup->addAction(d->mActualSizeAction);
+    actionGroup->setExclusive(true);
 
     // Adjust sizes
     int width = qMax(d->mZoomToFitButton->sizeHint().width(), d->mActualSizeButton->sizeHint().width());
     d->mZoomToFitButton->setFixedWidth(width);
     d->mActualSizeButton->setFixedWidth(width);
+    d->mZoomToFitWidthButton->setFixedWidth(width);
 }
 
 void ZoomWidget::slotZoomSliderActionTriggered()

lib/documentview/abstractdocumentviewadapter.h
@@ -92,6 +92,15 @@ public:
         return false;
     }
 
+    // Implementation must emit zoomToFitWidthChanged()
+    virtual void setZoomToFitWidth(bool)
+    {}
+
+    virtual bool zoomToFitWidth() const
+    {
+        return false;
+    }
+
     virtual qreal zoom() const
     {
         return 0;
@@ -101,6 +110,11 @@ public:
     {}
 
     virtual qreal computeZoomToFit() const
+    {
+        return 1.;
+    }
+
+    virtual qreal computeZoomToFitWidth() const
     {
         return 1.;
     }
@@ -139,6 +153,8 @@ Q_SIGNALS:
     void zoomChanged(qreal);
 
     void zoomToFitChanged(bool);
+
+    void zoomToFitWidthChanged(bool);
 
     void zoomInRequested(const QPointF&);
 

lib/documentview/abstractimageview.cpp
@@ -51,6 +51,7 @@ struct AbstractImageViewPrivate
 
     qreal mZoom;
     bool mZoomToFit;
+    bool mZoomToFitWidth;
     QPointF mImageOffset;
     QPointF mScrollPos;
     QPointF mLastDragPos;
@@ -119,6 +120,7 @@ AbstractImageView::AbstractImageView(QGraphicsItem* parent)
     d->mEnlargeSmallerImages = false;
     d->mZoom = 1;
     d->mZoomToFit = true;
+    d->mZoomToFitWidth = false;
     d->mImageOffset = QPointF(0, 0);
     d->mScrollPos = QPointF(0, 0);
     setFocusPolicy(Qt::WheelFocus);
@@ -226,6 +228,11 @@ bool AbstractImageView::zoomToFit() const
     return d->mZoomToFit;
 }
 
+bool AbstractImageView::zoomToFitWidth() const
+{
+    return d->mZoomToFitWidth;
+}
+
 void AbstractImageView::setZoomToFit(bool on)
 {
     d->mZoomToFit = on;
@@ -238,6 +245,18 @@ void AbstractImageView::setZoomToFit(bool on)
     zoomToFitChanged(d->mZoomToFit);
 }
 
+void AbstractImageView::setZoomToFitWidth(bool on)
+{
+    d->mZoomToFitWidth = on;
+    if (on) {
+        setZoom(computeZoomToFitWidth());
+    }
+    // We do not set zoom to 1 if zoomToFit is off, this is up to the code
+    // calling us. It may went to zoom to some other level and/or to zoom on
+    // a particular position
+    zoomToFitWidthChanged(d->mZoomToFitWidth);
+}
+
 void AbstractImageView::resizeEvent(QGraphicsSceneResizeEvent* event)
 {
     QGraphicsWidget::resizeEvent(event);
@@ -246,6 +265,13 @@ void AbstractImageView::resizeEvent(QGraphicsSceneResizeEvent* event)
         // If the view is resized but does not cause a zoom change, we call
         // adjustImageOffset() ourself.
         const qreal newZoom = computeZoomToFit();
+        if (qFuzzyCompare(zoom(), newZoom)) {
+            d->adjustImageOffset(AbstractImageViewPrivate::Notify);
+        } else {
+            setZoom(newZoom);
+        }
+    } else if (d->mZoomToFitWidth) {
+        const qreal newZoom = computeZoomToFitWidth();
         if (qFuzzyCompare(zoom(), newZoom)) {
             d->adjustImageOffset(AbstractImageViewPrivate::Notify);
         } else {
@@ -271,6 +297,20 @@ qreal AbstractImageView::computeZoomToFit() const
         fit = qMin(fit, qreal(1.));
     }
     return fit;
+}
+
+qreal AbstractImageView::computeZoomToFitWidth() const
+{
+    QSizeF docSize = documentSize();
+    if (docSize.isEmpty()) {
+        return 1;
+    }
+    QSizeF viewSize = boundingRect().size();
+    qreal fitWidth = viewSize.width() / docSize.width();
+    if (!d->mEnlargeSmallerImages) {
+        fitWidth = qMin(fitWidth, qreal(1.));
+    }
+    return fitWidth;
 }
 
 void AbstractImageView::mousePressEvent(QGraphicsSceneMouseEvent* event)

lib/documentview/abstractimageview.h
@@ -53,13 +53,19 @@ public:
 
     bool zoomToFit() const;
 
+    bool zoomToFitWidth() const;
+
     virtual void setZoomToFit(bool value);
+
+    virtual void setZoomToFitWidth(bool value);
 
     virtual void setDocument(Document::Ptr doc);
 
     Document::Ptr document() const;
 
     qreal computeZoomToFit() const;
+
+    qreal computeZoomToFitWidth() const;
 
     QSizeF documentSize() const;
 
@@ -98,6 +104,7 @@ public Q_SLOTS:
 
 Q_SIGNALS:
     void zoomToFitChanged(bool);
+    void zoomToFitWidthChanged(bool);
     void zoomChanged(qreal);
     void zoomInRequested(const QPointF&);
     void zoomOutRequested(const QPointF&);

lib/documentview/documentview.cpp
@@ -113,6 +113,8 @@ struct DocumentViewPrivate
                              q, SLOT(zoomOut(QPointF)));
             QObject::connect(adapter, SIGNAL(zoomToFitChanged(bool)),
                              q, SIGNAL(zoomToFitChanged(bool)));
+            QObject::connect(adapter, SIGNAL(zoomToFitWidthChanged(bool)),
+                             q, SIGNAL(zoomToFitWidthChanged(bool)));
         }
         QObject::connect(adapter, SIGNAL(scrollPosChanged()),
                          q, SIGNAL(positionChanged()));
@@ -134,7 +136,8 @@ struct DocumentViewPrivate
 
         if (mSetup.valid && adapter->canZoom()) {
             adapter->setZoomToFit(mSetup.zoomToFit);
-            if (!mSetup.zoomToFit) {
+            adapter->setZoomToFitWidth(mSetup.zoomToFitWidth);
+            if (!mSetup.zoomToFit && !mSetup.zoomToFitWidth) {
                 adapter->setZoom(mSetup.zoom);
                 adapter->setScrollPos(mSetup.position);
             }
@@ -144,6 +147,8 @@ struct DocumentViewPrivate
         if (adapter->canZoom()) {
             if (adapter->zoomToFit()) {
                 q->zoomToFitChanged(true);
+            } else if (adapter->zoomToFitWidth()) {
+                q->zoomToFitWidthChanged(true);
             } else {
                 q->zoomChanged(adapter->zoom());
             }
@@ -237,9 +242,17 @@ struct DocumentViewPrivate
         }
     }
 
+    void uncheckZoomToFitWidth()
+    {
+        if (mAdapter->zoomToFitWidth()) {
+            mAdapter->setZoomToFitWidth(false);
+        }
+    }
+
     void setZoom(qreal zoom, const QPointF& center = QPointF(-1, -1))
     {
         uncheckZoomToFit();
+        uncheckZoomToFitWidth();
         zoom = qBound(q->minimumZoom(), zoom, MAXIMUM_ZOOM_VALUE);
         mAdapter->setZoom(zoom, center);
     }
@@ -441,6 +454,11 @@ void DocumentView::slotCompleted()
         if (d->mAdapter->zoom() < min) {
             d->mAdapter->setZoom(min);
         }
+    } else if (!d->mAdapter->zoomToFitWidth()) {
+        qreal min = minimumZoom();
+        if (d->mAdapter->zoom() < min) {
+            d->mAdapter->setZoom(min);
+        }
     }
     emit completed();
 }
@@ -451,7 +469,8 @@ DocumentView::Setup DocumentView::setup() const
     if (d->mAdapter->canZoom()) {
         setup.valid = true;
         setup.zoomToFit = zoomToFit();
-        if (!setup.zoomToFit) {
+        setup.zoomToFitWidth = zoomToFitWidth();
+        if (!setup.zoomToFit && !setup.zoomToFitWidth) {
             setup.zoom = zoom();
             setup.position = position();
         }
@@ -481,9 +500,14 @@ void DocumentView::setZoomToFit(bool on)
         return;
     }
     d->mAdapter->setZoomToFit(on);
-    if (!on) {
-        d->mAdapter->setZoom(1.);
+}
+
+void DocumentView::setZoomToFitWidth(bool on)
+{
+    if (on == d->mAdapter->zoomToFitWidth()) {
+        return;
     }
+    d->mAdapter->setZoomToFitWidth(on);
 }
 
 bool DocumentView::zoomToFit() const
@@ -491,9 +515,15 @@ bool DocumentView::zoomToFit() const
     return d->mAdapter->zoomToFit();
 }
 
+bool DocumentView::zoomToFitWidth() const
+{
+    return d->mAdapter->zoomToFitWidth();
+}
+
 void DocumentView::zoomActualSize()
 {
     d->uncheckZoomToFit();
+    d->uncheckZoomToFitWidth();
     d->mAdapter->setZoom(1.);
 }
 

lib/documentview/documentview.h
@@ -51,6 +51,7 @@ class GWENVIEWLIB_EXPORT DocumentView : public QGraphicsWidget
     Q_OBJECT
     Q_PROPERTY(qreal zoom READ zoom WRITE setZoom NOTIFY zoomChanged)
     Q_PROPERTY(bool zoomToFit READ zoomToFit WRITE setZoomToFit NOTIFY zoomToFitChanged)
+    Q_PROPERTY(bool zoomToFitWidth READ zoomToFitWidth WRITE setZoomToFitWidth NOTIFY zoomToFitWidthChanged)
     Q_PROPERTY(QPoint position READ position WRITE setPosition NOTIFY positionChanged)
 public:
     static const int MaximumZoom;
@@ -59,11 +60,13 @@ public:
     struct Setup {
         Setup()
         : valid(false)
-        , zoomToFit(false)
+        , zoomToFit(true)
+        , zoomToFitWidth(false)
         , zoom(0)
         {}
         bool valid:1;
         bool zoomToFit:1;
+        bool zoomToFitWidth:1;
         qreal zoom;
         QPointF position;
     };
@@ -109,6 +112,8 @@ public:
 
     bool zoomToFit() const;
 
+    bool zoomToFitWidth() const;
+
     QPoint position() const;
 
     /**
@@ -142,6 +147,8 @@ public Q_SLOTS:
 
     void setZoomToFit(bool);
 
+    void setZoomToFitWidth(bool);
+
     void setPosition(const QPoint&);
 
     void hideAndDeleteLater();
@@ -171,6 +178,8 @@ Q_SIGNALS:
     void focused(DocumentView*);
 
     void zoomToFitChanged(bool);
+
+    void zoomToFitWidthChanged(bool);
 
     void positionChanged();
 

lib/documentview/documentviewcontroller.cpp
@@ -81,6 +81,7 @@ struct DocumentViewControllerPrivate
     ToolContainerContent* mToolContainerContent;
 
     QAction * mZoomToFitAction;
+    QAction * mZoomToFitWidthAction;
     QAction * mActualSizeAction;
     QAction * mZoomInAction;
     QAction * mZoomOutAction;
@@ -98,14 +99,24 @@ struct DocumentViewControllerPrivate
         mZoomToFitAction->setIcon(QIcon::fromTheme("zoom-fit-best"));
         mZoomToFitAction->setIconText(i18nc("@action:button Zoom to fit, shown in status bar, keep it short please", "Fit"));
 
+        mZoomToFitWidthAction = view->addAction("view_zoom_to_fit_width");
+        //view->collection()->setDefaultShortcuts(mZoomToFitWidthAction, Qt::Key_W); ??
+        mZoomToFitWidthAction->setCheckable(true);
+        mZoomToFitWidthAction->setChecked(false);
+        mZoomToFitWidthAction->setText(i18n("Zoom to Fit Width"));
+        mZoomToFitWidthAction->setIcon(QIcon::fromTheme("zoom-fit-best"));
+        mZoomToFitWidthAction->setIconText(i18nc("@action:button Zoom to fit width, shown in status bar, keep it short please", "Fit Width"));
+
         mActualSizeAction = view->addAction(KStandardAction::ActualSize);
+        mActualSizeAction->setCheckable(true);
+        mZoomToFitWidthAction->setChecked(false);
         mActualSizeAction->setIcon(QIcon::fromTheme("zoom-original"));
         mActualSizeAction->setIconText(i18nc("@action:button Zoom to original size, shown in status bar, keep it short please", "100%"));
 
         mZoomInAction = view->addAction(KStandardAction::ZoomIn);
         mZoomOutAction = view->addAction(KStandardAction::ZoomOut);
 
-        mActions << mZoomToFitAction << mActualSizeAction << mZoomInAction << mZoomOutAction;
+        mActions << mZoomToFitAction << mActualSizeAction << mZoomInAction << mZoomOutAction << mZoomToFitWidthAction;
     }
 
     void connectZoomWidget()
@@ -177,10 +188,13 @@ void DocumentViewController::setView(DocumentView* view)
     }
     connect(d->mView, &DocumentView::adapterChanged, this, &DocumentViewController::slotAdapterChanged);
     connect(d->mView, &DocumentView::zoomToFitChanged, this, &DocumentViewController::updateZoomToFitActionFromView);
+    connect(d->mView, &DocumentView::zoomToFitWidthChanged, this, &DocumentViewController::updateZoomToFitWidthActionFromView);
     connect(d->mView, &DocumentView::currentToolChanged, this, &DocumentViewController::updateTool);
 
     connect(d->mZoomToFitAction, SIGNAL(toggled(bool)),
             d->mView, SLOT(setZoomToFit(bool)));
+    connect(d->mZoomToFitWidthAction, SIGNAL(toggled(bool)),
+            d->mView, SLOT(setZoomToFitWidth(bool)));
     connect(d->mActualSizeAction, SIGNAL(triggered()),
             d->mView, SLOT(zoomActualSize()));
     connect(d->mZoomInAction, SIGNAL(triggered()),
@@ -190,6 +204,7 @@ void DocumentViewController::setView(DocumentView* view)
 
     d->updateActions();
     updateZoomToFitActionFromView();
+    updateZoomToFitWidthActionFromView();
     updateTool();
 
     // Sync zoom widget
@@ -210,7 +225,8 @@ void DocumentViewController::setZoomWidget(ZoomWidget* widget)
         d->mZoomToFitAction,
         d->mActualSizeAction,
         d->mZoomInAction,
-        d->mZoomOutAction
+        d->mZoomOutAction,
+        d->mZoomToFitWidthAction
     );
 
     d->mZoomWidget->setMaximumZoom(qreal(DocumentView::MaximumZoom));
@@ -234,6 +250,12 @@ void DocumentViewController::updateZoomToFitActionFromView()
 {
     SignalBlocker blocker(d->mZoomToFitAction);
     d->mZoomToFitAction->setChecked(d->mView->zoomToFit());
+}
+
+void DocumentViewController::updateZoomToFitWidthActionFromView()
+{
+    SignalBlocker blocker(d->mZoomToFitWidthAction);
+    d->mZoomToFitWidthAction->setChecked(d->mView->zoomToFitWidth());
 }
 
 void DocumentViewController::updateTool()

lib/documentview/documentviewcontroller.h
@@ -63,6 +63,7 @@ public:
 private Q_SLOTS:
     void slotAdapterChanged();
     void updateZoomToFitActionFromView();
+    void updateZoomToFitWidthActionFromView();
     void updateTool();
 
 private:

commit 7389aa55dbdf9c8af95106498aa6ce082cc03eb2

Fix padding of "Fit Width" button
Summary:
3e10699ac37c4 introduced "Fit to Width", but failed to adapt the width
calculation of the zoom mode buttons in the bottom zoom widget bar
accordingly. This resulted in the middle button getting its text cut off
for some languages, looking like this:

{F5431466}

There are several options to remedy this:
- Give equal width to all buttons
- Give equal width to the outer buttons (i.e. symmetric, probably the initial intention when there were only two buttons)
- Just respect the size hint of each button individually

| {F5431473} | {F5431474} | {F5431476} |

As the three buttons are already quite wide, in order not to impact the
minimal window size too much (rules out "equal width") and at the same
time not to make the "Fit" button for English too narrow (rules out
"individual width") we choose the "symmetric" option.

BUG: 385035

Test Plan:
Switch application language to Czech, view image. Text in "Fit to Width"
(i.e. middle) button not cut off anymore, minimal window size only
slightly impacted. For English, the "Fit" button is still wide enough.

Reviewers: #kde_applications, broulik

Reviewed By: broulik

Subscribers: broulik

Differential Revision: https://phabricator.kde.org/D8306

lib/zoomwidget.cpp
@@ -157,7 +157,6 @@ void ZoomWidget::setActions(QAction* zoomToFitAction, QAction* actualSizeAction,
     int width = qMax(d->mZoomToFitButton->sizeHint().width(), d->mActualSizeButton->sizeHint().width());
     d->mZoomToFitButton->setFixedWidth(width);
     d->mActualSizeButton->setFixedWidth(width);
-    d->mZoomToFitWidthButton->setFixedWidth(width);
 }
 
 void ZoomWidget::slotZoomSliderActionTriggered()