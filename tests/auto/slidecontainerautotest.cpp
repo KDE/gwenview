eaa5eead0c2da0ac8690358fe03869b03ff30a97
Montel Laurent <montel@kde.org>
2017-12-06 08:54:13 +0100

    Check return value

--- tests/auto/slidecontainerautotest.cpp
+++ tests/auto/slidecontainerautotest.cpp
@@ -136,7 +136,7 @@ void SlideContainerAutoTest::testHiddenContentResize()
     // Resizing content should not trigger a slide if it is not visible.
     TestWindow window;
     window.show();
-    QTest::qWaitForWindowExposed(&window);
+    QVERIFY(QTest::qWaitForWindowExposed(&window));
 
     window.mContent->show();
     window.mContent->setFixedSize(150, 80);
