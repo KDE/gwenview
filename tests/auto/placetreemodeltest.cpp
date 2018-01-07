KDE/kio

commit 7eb6333bdb48f39d592b07c565eb8a7d02cbf0d3

Added baloo urls into places model
Summary:
Added new sections for places model: 'Recently Saved' and 'Search for' with baloo urls

Based on dolphin code

{F5435566}

Depends on D8243

Test Plan:
From an kde app try to open/save a file.
It should show a new file dialog with new categories on the places panel

Reviewers: #frameworks, #dolphin, #kde_applications, dvratil, #vdg, ngraham, ervin, mlaurent, dfaure, mwolff

Reviewed By: #dolphin, ngraham, ervin, mlaurent, dfaure, mwolff

Subscribers: mwolff, dfaure, ervin, usta, mlaurent, dvratil, ngraham, #frameworks

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D8332

autotests/CMakeLists.txt
@@ -119,6 +119,7 @@ ecm_add_tests(
  knewfilemenutest.cpp
  kfilecopytomenutest.cpp
  kfileplacesmodeltest.cpp
+ kfileplacesviewtest.cpp
  kurlrequestertest.cpp
  NAME_PREFIX "kiofilewidgets-"
  LINK_LIBRARIES KF5::KIOFileWidgets KF5::KIOWidgets KF5::XmlGui KF5::Bookmarks Qt5::Test KF5::I18n

autotests/kfileplacesmodeltest.cpp
@@ -25,6 +25,8 @@
 #include <QDebug>
 #include <kfileplacesmodel.h>
 #include <solid/device.h>
+#include <kconfig.h>
+#include <kconfiggroup.h>
 
 #include <QtTest/QtTest>
 
@@ -66,6 +68,7 @@ private Q_SLOTS:
     void testDevicePlugging();
     void testDragAndDrop();
     void testDeviceSetupTeardown();
+    void testEnableBaloo();
 
 private:
     QStringList placesUrls() const;
@@ -92,6 +95,12 @@ void KFilePlacesModelTest::initTestCase()
 
     // Ensure we'll have a clean bookmark file to start
     QFile::remove(bookmarksFile());
+
+    // disable baloo by default
+    KConfig config(QStringLiteral("baloofilerc"));
+    KConfigGroup basicSettings = config.group("Basic Settings");
+    basicSettings.writeEntry("Indexing-Enabled", false);
+    config.sync();
 
     qRegisterMetaType<QModelIndex>();
     const QString fakeHw = QFINDTESTDATA("fakecomputer.xml");
@@ -694,6 +703,31 @@ void KFilePlacesModelTest::testDeviceSetupTeardown()
     args = spy_changed.takeFirst();
     QCOMPARE(args.at(0).toModelIndex().row(), 6);
     QCOMPARE(args.at(1).toModelIndex().row(), 6);
+}
+
+void KFilePlacesModelTest::testEnableBaloo()
+{
+    KConfig config(QStringLiteral("baloofilerc"));
+    KConfigGroup basicSettings = config.group("Basic Settings");
+    basicSettings.writeEntry("Indexing-Enabled", true);
+    config.sync();
+
+    KFilePlacesModel places_with_baloo;
+    QStringList urls;
+     for (int row = 0; row < places_with_baloo.rowCount(); ++row) {
+         QModelIndex index = places_with_baloo.index(row, 0);
+         urls << places_with_baloo.url(index).toDisplayString(QUrl::PreferLocalFile);
+    }
+
+    QVERIFY(urls.contains("timeline:/today"));
+    QVERIFY(urls.contains("timeline:/yesterday"));
+    QVERIFY(urls.contains("timeline:/thismonth"));
+    QVERIFY(urls.contains("timeline:/lastmonth"));
+
+    QVERIFY(urls.contains("search:/documents"));
+    QVERIFY(urls.contains("search:/images"));
+    QVERIFY(urls.contains("search:/audio"));
+    QVERIFY(urls.contains("search:/videos"));
 }
 
 QTEST_MAIN(KFilePlacesModelTest)

autotests/kfileplacesviewtest.cpp
@@ -1,0 +1,122 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2017 Renato Araujo Oliveira Filho <renato.araujo@kdab.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+
+*/
+
+#include <QObject>
+#include <QDebug>
+#include <QStandardPaths>
+#include <QFile>
+
+#include <kfileplacesview.h>
+#include <kfileplacesmodel.h>
+#include <kconfig.h>
+#include <kconfiggroup.h>
+
+#include <QTest>
+#include <QSignalSpy>
+
+static QString bookmarksFile()
+{
+    return QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + "/user-places.xbel";
+}
+
+class KFilePlacesViewTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
+
+    void testUrlChanged_data();
+    void testUrlChanged();
+};
+
+void KFilePlacesViewTest::initTestCase()
+{
+    qputenv("KDE_FORK_SLAVES", "yes"); // to avoid a runtime dependency on klauncher
+    QStandardPaths::setTestModeEnabled(true);
+
+    cleanupTestCase();
+
+    KConfig config(QStringLiteral("baloofilerc"));
+    KConfigGroup basicSettings = config.group("Basic Settings");
+    basicSettings.writeEntry("Indexing-Enabled", true);
+    config.sync();
+
+    qRegisterMetaType<QModelIndex>();
+}
+
+void KFilePlacesViewTest::cleanupTestCase()
+{
+    QFile::remove(bookmarksFile());
+}
+
+void KFilePlacesViewTest::testUrlChanged_data()
+{
+    QTest::addColumn<int>("row");
+    QTest::addColumn<QString>("expectedUrl");
+
+    const QDate currentDate = QDate::currentDate();
+    const QDate yesterdayDate = currentDate.addDays(-1);
+    const QDate lastMonthDate = currentDate.addMonths(-1);
+    QTest::newRow("Today") << 4 << QStringLiteral("timeline:/today");
+    QTest::newRow("Yesterday") << 5 << QString("timeline:/%1-%2/%1-%2-%3")
+                                  .arg(yesterdayDate.year())
+                                  .arg(yesterdayDate.month(), 2, 10, QChar('0'))
+                                  .arg(yesterdayDate.day(), 2, 10, QChar('0'));
+
+    QTest::newRow("This Month") << 6 << QString("timeline:/%1-%2")
+                                   .arg(currentDate.year())
+                                   .arg(currentDate.month(), 2, 10, QChar('0'));
+    QTest::newRow("Last Month") << 7 << QString("timeline:/%1-%2")
+                                   .arg(lastMonthDate.year())
+                                   .arg(lastMonthDate.month(), 2, 10, QChar('0'));
+
+    // search
+    const QString baloonurl = QStringLiteral("baloosearch:?json=%7B%22dayFilter%22: 0, %22monthFilter%22: 0, %22yearFilter%22: 0, %22type%22: [ %22$TYPE$%22]%7D");
+    const QString typeToReplace = QStringLiteral("$TYPE$");
+
+    QTest::newRow("Documents") << 8 << QString(baloonurl).replace(typeToReplace, QStringLiteral("Document"));
+    QTest::newRow("Images") << 9 << QString(baloonurl).replace(typeToReplace, QStringLiteral("Image"));
+    QTest::newRow("Audio Files") << 10 << QString(baloonurl).replace(typeToReplace, QStringLiteral("Audio"));
+    QTest::newRow("Videos") << 11 << QString(baloonurl).replace(typeToReplace, QStringLiteral("Video"));
+}
+
+void KFilePlacesViewTest::testUrlChanged()
+{
+    QFETCH(int, row);
+    QFETCH(QString, expectedUrl);
+
+    KFilePlacesView pv;
+    pv.show();
+    pv.setModel(new KFilePlacesModel());
+    QTest::qWaitForWindowActive(&pv);
+
+    QSignalSpy urlChangedSpy(&pv, &KFilePlacesView::urlChanged);
+    const QModelIndex targetIndex = pv.model()->index(row, 0);
+    pv.scrollTo(targetIndex);
+    pv.clicked(targetIndex);
+    QTRY_COMPARE(urlChangedSpy.count(), 1);
+    const QList<QVariant> args = urlChangedSpy.takeFirst();
+    QCOMPARE(args.at(0).toUrl().toString(), expectedUrl);
+}
+
+QTEST_MAIN(KFilePlacesViewTest)
+
+#include "kfileplacesviewtest.moc"

src/filewidgets/kfileplacesmodel.cpp
@@ -46,6 +46,9 @@
 #include <kio/job.h>
 #include <kprotocolinfo.h>
 
+#include <kconfig.h>
+#include <kconfiggroup.h>
+
 #include <solid/devicenotifier.h>
 #include <solid/storageaccess.h>
 #include <solid/storagedrive.h>
@@ -56,10 +59,25 @@
 #include <solid/predicate.h>
 #include <qstandardpaths.h>
 
+namespace {
+    static bool isFileIndexingEnabled()
+    {
+        KConfig config(QStringLiteral("baloofilerc"));
+        KConfigGroup basicSettings = config.group("Basic Settings");
+        return basicSettings.readEntry("Indexing-Enabled", true);
+    }
+}
+
 class Q_DECL_HIDDEN KFilePlacesModel::Private
 {
 public:
-    Private(KFilePlacesModel *self) : q(self), bookmarkManager(nullptr) {}
+    Private(KFilePlacesModel *self)
+        : q(self),
+          bookmarkManager(nullptr),
+          fileIndexingEnabled(isFileIndexingEnabled())
+    {
+    }
+
     ~Private()
     {
         qDeleteAll(items);
@@ -74,6 +92,8 @@ public:
     Solid::Predicate predicate;
     KBookmarkManager *bookmarkManager;
 
+    const bool fileIndexingEnabled;
+
     void reloadAndSignal();
     QList<KFilePlacesItem *> loadBookmarkList();
 
@@ -84,6 +104,9 @@ public:
     void _k_reloadBookmarks();
     void _k_storageSetupDone(Solid::ErrorType error, QVariant errorData);
     void _k_storageTeardownDone(Solid::ErrorType error, QVariant errorData);
+
+private:
+    bool isBalooUrl(const QUrl &url) const;
 };
 
 KFilePlacesModel::KFilePlacesModel(QObject *parent)
@@ -131,6 +154,39 @@ KFilePlacesModel::KFilePlacesModel(QObject *parent)
         // will always return false, which opening/closing all the time the open/save dialog would case the
         // bookmarks to be added once each time, having lots of times each bookmark. (ereslibre)
         d->bookmarkManager->saveAs(file);
+    }
+
+    // if baloo is enabled, add new urls even if the bookmark file is not empty
+    if (d->fileIndexingEnabled &&
+        root.metaDataItem(QStringLiteral("withBaloo")) != QLatin1String("true")) {
+
+        root.setMetaDataItem(QStringLiteral("withBaloo"), QStringLiteral("true"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Today"), I18N_NOOP2("KFile System Bookmarks", "Today"),
+                                              QUrl(QStringLiteral("timeline:/today")),  QStringLiteral("go-jump-today"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Yesterday"), I18N_NOOP2("KFile System Bookmarks", "Yesterday"),
+                                              QUrl(QStringLiteral("timeline:/yesterday")),  QStringLiteral("view-calendar-day"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("This Month"), I18N_NOOP2("KFile System Bookmarks", "This Month"),
+                                              QUrl(QStringLiteral("timeline:/thismonth")),  QStringLiteral("view-calendar-month"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Last Month"), I18N_NOOP2("KFile System Bookmarks", "Last Month"),
+                                              QUrl(QStringLiteral("timeline:/lastmonth")),  QStringLiteral("view-calendar-month"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Documents"), I18N_NOOP2("KFile System Bookmarks", "Documents"),
+                                             QUrl(QStringLiteral("search:/documents")),  QStringLiteral("folder-text"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Images"), I18N_NOOP2("KFile System Bookmarks", "Images"),
+                                              QUrl(QStringLiteral("search:/images")),  QStringLiteral("folder-images"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Audio Files"), I18N_NOOP2("KFile System Bookmarks", "Audio Files"),
+                                              QUrl(QStringLiteral("search:/audio")),  QStringLiteral("folder-sound"));
+        KFilePlacesItem::createSystemBookmark(d->bookmarkManager,
+                                              QStringLiteral("Videos"), I18N_NOOP2("KFile System Bookmarks", "Videos"),
+                                              QUrl(QStringLiteral("search:/videos")),  QStringLiteral("folder-videos"));
+
+        d->bookmarkManager->save();
     }
 
     QString predicate(QString::fromLatin1("[[[[ StorageVolume.ignored == false AND [ StorageVolume.usage == 'FileSystem' OR StorageVolume.usage == 'Encrypted' ]]"
@@ -422,6 +478,13 @@ void KFilePlacesModel::Private::_k_reloadBookmarks()
     currentItems.clear();
 }
 
+bool KFilePlacesModel::Private::isBalooUrl(const QUrl &url) const
+{
+    const QString scheme = url.scheme();
+    return ((scheme == QLatin1String("timeline")) ||
+            (scheme == QLatin1String("search")));
+}
+
 QList<KFilePlacesItem *> KFilePlacesModel::Private::loadBookmarkList()
 {
     QList<KFilePlacesItem *> items;
@@ -433,15 +496,18 @@ QList<KFilePlacesItem *> KFilePlacesModel::Private::loadBookmarkList()
     while (!bookmark.isNull()) {
         QString udi = bookmark.metaDataItem(QStringLiteral("UDI"));
         QString appName = bookmark.metaDataItem(QStringLiteral("OnlyInApp"));
+        QUrl url = bookmark.url();
         auto it = std::find(devices.begin(), devices.end(), udi);
         bool deviceAvailable = (it != devices.end());
         if (it != devices.end()) {
             devices.erase(it);
         }
 
-        bool allowedHere = appName.isEmpty() || (appName == QCoreApplication::instance()->applicationName());
+        bool allowedHere = appName.isEmpty() || (appName == QCoreApplication::instance()->applicationName());        
+        bool isSupportedUrl = isBalooUrl(url) ? fileIndexingEnabled : true;
 
-        if ((udi.isEmpty() && allowedHere) || deviceAvailable) {
+        if ((isSupportedUrl && udi.isEmpty() && allowedHere) || deviceAvailable) {
+
             KFilePlacesItem *item;
             if (deviceAvailable) {
                 item = new KFilePlacesItem(bookmarkManager, bookmark.address(), udi);

src/filewidgets/kfileplacesview.cpp
@@ -29,6 +29,7 @@
 #include <QApplication>
 #include <QMenu>
 #include <QScrollBar>
+#include <QUrlQuery>
 
 #include <QDebug>
 
@@ -494,6 +495,13 @@ public:
     int insertIndicatorHeight(int itemHeight) const;
     void fadeCapacityBar(const QModelIndex &index, FadeType fadeType);
     int sectionsCount() const;
+
+    // based on dolphin urls
+    QUrl convertUrl(const QUrl &url) const;
+    QUrl createTimelineUrl(const QUrl &url) const;
+    QUrl createSearchUrl(const QUrl &url) const;
+    QString timelineDateString(int year, int month, int day = 0) const;
+    QUrl searchUrlForType(const QString &type) const;
 
     void _k_placeClicked(const QModelIndex &index);
     void _k_placeEntered(const QModelIndex &index);
@@ -1086,7 +1094,7 @@ void KFilePlacesView::Private::setCurrentIndex(const QModelIndex &index)
     if (url.isValid()) {
         currentUrl = url;
         updateHiddenRows();
-        emit q->urlChanged(url);
+        emit q->urlChanged(convertUrl(url));
         if (showAll) {
             q->setShowAll(false);
         }
@@ -1270,6 +1278,98 @@ int KFilePlacesView::Private::sectionsCount() const
 
     return count;
 }
+
+QUrl KFilePlacesView::Private::convertUrl(const QUrl &url) const
+{
+    QUrl newUrl = url;
+    if (url.scheme() == QLatin1String("timeline")) {
+        newUrl = createTimelineUrl(url);
+    } else if (url.scheme() == QLatin1String("search")) {
+        newUrl = createSearchUrl(url);
+    }
+
+    return newUrl;
+}
+
+QUrl KFilePlacesView::Private::createTimelineUrl(const QUrl &url) const
+{
+    // based on dolphin urls
+    const QString timelinePrefix = QStringLiteral("timeline:") + QLatin1Char('/');
+    QUrl timelineUrl;
+
+    const QString path = url.toDisplayString(QUrl::PreferLocalFile);
+    if (path.endsWith(QLatin1String("/yesterday"))) {
+        const QDate date = QDate::currentDate().addDays(-1);
+        const int year = date.year();
+        const int month = date.month();
+        const int day = date.day();
+
+        timelineUrl = QUrl(timelinePrefix + timelineDateString(year, month) +
+              '/' + timelineDateString(year, month, day));
+    } else if (path.endsWith(QLatin1String("/thismonth"))) {
+        const QDate date = QDate::currentDate();
+        timelineUrl = QUrl(timelinePrefix + timelineDateString(date.year(), date.month()));
+    } else if (path.endsWith(QLatin1String("/lastmonth"))) {
+        const QDate date = QDate::currentDate().addMonths(-1);
+        timelineUrl = QUrl(timelinePrefix + timelineDateString(date.year(), date.month()));
+    } else {
+        Q_ASSERT(path.endsWith(QLatin1String("/today")));
+        timelineUrl = url;
+    }
+
+    return timelineUrl;
+}
+
+QString KFilePlacesView::Private::timelineDateString(int year, int month, int day) const
+{
+    const QString dateFormat("%1-%2");
+
+    QString date = dateFormat.arg(year).arg(month, 2, 10, QChar('0'));
+    if (day > 0) {
+        date += QString("-%1").arg(day, 2, 10, QChar('0'));
+    }
+    return date;
+}
+
+QUrl KFilePlacesView::Private::createSearchUrl(const QUrl &url) const
+{
+    QUrl searchUrl;
+
+    const QString path = url.toDisplayString(QUrl::PreferLocalFile);
+
+    if (path.endsWith(QLatin1String("/documents"))) {
+        searchUrl = searchUrlForType(QStringLiteral("Document"));
+    } else if (path.endsWith(QLatin1String("/images"))) {
+        searchUrl = searchUrlForType(QStringLiteral("Image"));
+    } else if (path.endsWith(QLatin1String("/audio"))) {
+        searchUrl = searchUrlForType(QStringLiteral("Audio"));
+    } else if (path.endsWith(QLatin1String("/videos"))) {
+        searchUrl = searchUrlForType(QStringLiteral("Video"));
+    } else {
+        qWarning() << "Invalid search url:" << url;
+        searchUrl = url;
+    }
+
+    return searchUrl;
+}
+
+
+QUrl KFilePlacesView::Private::searchUrlForType(const QString& type) const
+{
+    const QString jsonQuery(QStringLiteral("{\"dayFilter\": 0,\
+                                             \"monthFilter\": 0, \
+                                             \"yearFilter\": 0, \
+                                             \"type\": [ \"%1\"]}"));
+    QUrl url;
+    url.setScheme(QStringLiteral("baloosearch"));
+
+    QUrlQuery urlQuery;
+    urlQuery.addQueryItem(QStringLiteral("json"), jsonQuery.arg(type).simplified());
+    url.setQuery(urlQuery);
+
+    return url;
+}
+
 
 void KFilePlacesView::Private::_k_placeClicked(const QModelIndex &index)
 {

KDE/gwenview

commit e8d799c9b522af299f721dbbfcc650526ba78940

Fix failing PlaceTreeModelTest autotest
Since Frameworks 5.41, `PlaceTreeModelTest` would fail. Bisecting shows
this has been caused by 7eb6333bdb48 in KIO ("Added baloo urls into
places model"), where the model now returns 4 additional entries for
"Recently Saved" and another 4 entries for "Search For".

While the fix is trivially done by changing the number of expected
items, in a way the test functioned as a canary for problems deeper in
the code. In particular it uncovered problems when accessing the newly
added virtual folders as well as various usability issues. Some of those
are fixed already (50e6fa3ffc49 and 86d754546bd1), but there is still
some work left to do.

BUG: 387824

Test Plan: `placetreemodeltest` does not fail with KF 5.41 anymore,
still works with KF 5.40.

tests/auto/placetreemodeltest.cpp
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <QStandardPaths>
 #include <QStandardPaths>
 #include <qtest.h>
+#include <kio_version.h>
 
 // Local
 #include "../lib/placetreemodel.h"
@@ -123,7 +124,12 @@ void PlaceTreeModelTest::init()
 void PlaceTreeModelTest::testListPlaces()
 {
     PlaceTreeModel model(0);
+
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 41, 0)
+    QCOMPARE(model.rowCount(), 10);
+#else
     QCOMPARE(model.rowCount(), 2);
+#endif
 
     QModelIndex index;
     index = model.index(0, 0);