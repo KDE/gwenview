KDE/gwenview

commit d7ae0b6f4d4063acf0407777853e262c460b8d08

Correctly crop Sony's embedded thumbnails
Reviewers: #gwenview, ngraham

Reviewed By: #gwenview, ngraham

Subscribers: ngraham, #gwenview

Tags: #gwenview

Differential Revision: https://phabricator.kde.org/D21398

lib/jpegcontent.cpp
@@ -574,13 +574,30 @@ QImage JpegContent::thumbnail() const
 #endif
         image.loadFromData(thumbnail.pData_, thumbnail.size_);
 
-        Exiv2::ExifKey key("Exif.Canon.ThumbnailImageValidArea");
-        Exiv2::ExifData::iterator it = d->mExifData.findKey(key);
-
+        Exiv2::ExifData::iterator it = d->mExifData.findKey(Exiv2::ExifKey("Exif.Canon.ThumbnailImageValidArea"));
         // ensure ThumbnailImageValidArea actually specifies a rectangle, i.e. there must be 4 coordinates
         if (it != d->mExifData.end() && it->count() == 4) {
             QRect validArea(QPoint(it->toLong(0), it->toLong(2)), QPoint(it->toLong(1), it->toLong(3)));
             image = image.copy(validArea);
+        } else {
+            // Unfortunately, Sony does not provide an exif tag that specifies the valid area of the 
+            // embedded thumbnail. Need to derive it from the size of the preview image instead.
+            it = d->mExifData.findKey(Exiv2::ExifKey("Exif.Sony1.PreviewImageSize"));
+            if (it != d->mExifData.end() && it->count() == 2) {
+                const long prevHeight = it->toLong(0);
+                const long prevWidth = it->toLong(1);
+
+                const double scale = prevWidth / image.width();
+
+                // the embedded thumb only needs to be cropped vertically
+                const long validThumbAreaHeight = ceil(prevHeight / scale);
+                const long totalHeightOfBlackArea = image.height() - validThumbAreaHeight;
+                // black bars on top and bottom should be equal in height
+                const long offsetFromTop = totalHeightOfBlackArea / 2;
+
+                const QRect validArea(QPoint(0, offsetFromTop), QSize(image.width(), validThumbAreaHeight));
+                image = image.copy(validArea);
+            }
         }
 
         Orientation o = orientation();

commit fdd5ecc344edc7444bd57a080c3d9ec91b245592

Fix divide-by-zero crash
Summary:
Bug was introduced in D21398.

BUG: 414631
FIXED-IN: 19.12.2

Test Plan:
Compile only and verify normal functionality only; can't reproduce the issue so can't
test the fix

Reviewers: #gwenview, cfeck, tommo

Subscribers: tommo

Tags: #gwenview

Differential Revision: https://phabricator.kde.org/D26527

lib/jpegcontent.cpp
@@ -588,16 +588,18 @@ QImage JpegContent::thumbnail() const
                 const long prevHeight = it->toLong(0);
                 const long prevWidth = it->toLong(1);
 
-                const double scale = prevWidth / image.width();
+                if (image.width() > 0 && prevWidth > 0 ) {
+                    const double scale = prevWidth / image.width();
 
-                // the embedded thumb only needs to be cropped vertically
-                const long validThumbAreaHeight = ceil(prevHeight / scale);
-                const long totalHeightOfBlackArea = image.height() - validThumbAreaHeight;
-                // black bars on top and bottom should be equal in height
-                const long offsetFromTop = totalHeightOfBlackArea / 2;
+                    // the embedded thumb only needs to be cropped vertically
+                    const long validThumbAreaHeight = ceil(prevHeight / scale);
+                    const long totalHeightOfBlackArea = image.height() - validThumbAreaHeight;
+                    // black bars on top and bottom should be equal in height
+                    const long offsetFromTop = totalHeightOfBlackArea / 2;
 
-                const QRect validArea(QPoint(0, offsetFromTop), QSize(image.width(), validThumbAreaHeight));
-                image = image.copy(validArea);
+                    const QRect validArea(QPoint(0, offsetFromTop), QSize(image.width(), validThumbAreaHeight));
+                    image = image.copy(validArea);
+                }
             }
         }
 