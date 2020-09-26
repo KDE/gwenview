KDE/gwenview

commit 4f7d35d1753262a9e6497c3b13ca74ff254d2ea3

Fix leaking ThumbnailGenerators
The logic for deleting ThumbnailGenerators was kind of broken, so fix
that.

Not sure if this is the right approach, but it doesn't seem racy (and
can't trigger any races), and doesn't leak anymore.

Test Plan: Run with asan and ubsan, open folders with many thumbnails,
scroll around.

Reviewed By: ngraham

Differential Revision: https://phabricator.kde.org/D28346

lib/thumbnailprovider/thumbnailgenerator.cpp
@@ -36,6 +36,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 #include <QImageReader>
 #include <QTransform>
 #include <QBuffer>
+#include <QCoreApplication>
 
 namespace Gwenview
 {
@@ -178,7 +179,12 @@ bool ThumbnailContext::load(const QString &pixPath, int pixelSize)
 //------------------------------------------------------------------------
 ThumbnailGenerator::ThumbnailGenerator()
 : mCancel(false)
-{}
+{
+    connect(qApp, &QCoreApplication::aboutToQuit, this, [=]() {
+        wait();
+    }, Qt::DirectConnection);
+    start();
+}
 
 void ThumbnailGenerator::load(
     const QString& originalUri, time_t originalTime, KIO::filesize_t originalFileSize, const QString& originalMimeType,
@@ -196,13 +202,18 @@ void ThumbnailGenerator::load(
     mPixPath = pixPath;
     mThumbnailPath = thumbnailPath;
     mThumbnailGroup = group;
-    if (!isRunning()) start();
     mCond.wakeOne();
 }
 
 QString ThumbnailGenerator::originalUri() const
 {
     return mOriginalUri;
+}
+
+bool ThumbnailGenerator::isStopped()
+{
+    QMutexLocker lock(&mMutex);
+    return mStopped;
 }
 
 time_t ThumbnailGenerator::originalTime() const
@@ -235,21 +246,18 @@ void ThumbnailGenerator::cancel()
 
 void ThumbnailGenerator::run()
 {
-    LOG("");
     while (!testCancel()) {
         QString pixPath;
         int pixelSize;
         {
             QMutexLocker lock(&mMutex);
             // empty mPixPath means nothing to do
-            LOG("Waiting for mPixPath");
             if (mPixPath.isNull()) {
-                LOG("mPixPath.isNull");
                 mCond.wait(&mMutex);
             }
         }
         if (testCancel()) {
-            return;
+            break;
         }
         {
             QMutexLocker lock(&mMutex);
@@ -279,7 +287,7 @@ void ThumbnailGenerator::run()
             mPixPath.clear(); // done, ready for next
         }
         if (testCancel()) {
-            return;
+            break;
         }
         {
             QSize size(mOriginalWidth, mOriginalHeight);
@@ -289,7 +297,12 @@ void ThumbnailGenerator::run()
             LOG("Done");
         }
     }
+
     LOG("Ending thread");
+
+    QMutexLocker lock(&mMutex);
+    mStopped = true;
+    deleteLater();
 }
 
 void ThumbnailGenerator::cacheThumbnail()

lib/thumbnailprovider/thumbnailgenerator.h
@@ -51,6 +51,10 @@ class ThumbnailGenerator : public QThread
 public:
     ThumbnailGenerator();
 
+    // Because we override run(), like you're not really supposed to do, we
+    // can't trust isRunning()
+    bool isStopped();
+
     void load(
         const QString& originalUri,
         time_t originalTime,
@@ -89,6 +93,7 @@ private:
     QWaitCondition mCond;
     ThumbnailGroup::Enum mThumbnailGroup;
     bool mCancel;
+    bool mStopped = false;
 };
 
 } // namespace

lib/thumbnailprovider/thumbnailprovider.cpp
@@ -175,11 +175,10 @@ ThumbnailProvider::ThumbnailProvider()
 ThumbnailProvider::~ThumbnailProvider()
 {
     LOG(this);
-    abortSubjob();
-    mThumbnailGenerator->cancel();
     disconnect(mThumbnailGenerator, nullptr, this, nullptr);
     disconnect(mThumbnailGenerator, nullptr, sThumbnailWriter, nullptr);
-    connect(mThumbnailGenerator, SIGNAL(finished()), mThumbnailGenerator, SLOT(deleteLater()));
+    abortSubjob();
+    mThumbnailGenerator->cancel();
     if (mPreviousThumbnailGenerator) {
         disconnect(mPreviousThumbnailGenerator, nullptr, sThumbnailWriter, nullptr);
     }
@@ -193,7 +192,7 @@ void ThumbnailProvider::stop()
     // startCreatingThumbnail() will take care that these two threads won't work on the same item.
     mItems.clear();
     abortSubjob();
-    if (mThumbnailGenerator->isRunning() && !mPreviousThumbnailGenerator) {
+    if (!mThumbnailGenerator->isStopped() && !mPreviousThumbnailGenerator) {
         mPreviousThumbnailGenerator = mThumbnailGenerator;
         mPreviousThumbnailGenerator->cancel();
         disconnect(mPreviousThumbnailGenerator, nullptr, this, nullptr);
@@ -524,7 +523,7 @@ void ThumbnailProvider::startCreatingThumbnail(const QString& pixPath)
     // connect mPreviousThumbnailGenerator's signal "finished" to determineNextIcon
     // which will load the thumbnail from sThumbnailWriter or from disk
     // (because we re-add mCurrentItem to mItems).
-    if (mPreviousThumbnailGenerator && mPreviousThumbnailGenerator->isRunning() &&
+    if (mPreviousThumbnailGenerator && !mPreviousThumbnailGenerator->isStopped() &&
         mOriginalUri == mPreviousThumbnailGenerator->originalUri() &&
         mOriginalTime == mPreviousThumbnailGenerator->originalTime() &&
         mOriginalFileSize == mPreviousThumbnailGenerator->originalFileSize() &&

commit d15622e1655ba7fcd33479f9677112340b2de7ff

Fix deadlock when closing gwenview importer
When gwenview_importer is closed after importing files, it gets into a
deadlock.  This bug was introduced in commit 4f7d35d1 by the refactoring
of thumbnailprovider and thumbnailgenerator.

The deadlock is caused by the fact that closing the application will
directly trigger a wait() in the thumbnailgenerator thread which will
wait on proper closure of this thread before allowing the application to
exit.  However, this thumbnailgenerator thread is, at that moment, in a
QWaitCondition state which is intended to wait on new thumbnails to be
generated.
As far as I can see, this is not a problem in the main gwenview
application because the deadlock will eventually be lifted by the
destruction of the thumbnailgenerator object by the chain of destructors
going from application level down to thumbnailprovider which is the
owner of this object.
In the importer the situation is a bit different.  After importing has
finished, the thumbnailpage which is the owner of thumbnailprovider
is not the current widget anymore.  It still exists in the background in
case the user presses the "Import more Documents" button.  For some
reason the destruction chain doesn't seem to properly trigger the
cancel() function of the thumbnailgenerator object/thread.

The simple solution is to call the cancel() function just before the
wait() function is called when the application emits aboutToQuit().
This is proper behavior, since the thumbnailgenerator should always be
triggered to close down when the application is quitting.

BUG: 425971

lib/thumbnailprovider/thumbnailgenerator.cpp
@@ -181,6 +181,7 @@ ThumbnailGenerator::ThumbnailGenerator()
 : mCancel(false)
 {
     connect(qApp, &QCoreApplication::aboutToQuit, this, [=]() {
+        cancel();
         wait();
     }, Qt::DirectConnection);
     start();