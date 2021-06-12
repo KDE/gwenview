5e20e12b8b4cb89e22e388cf99509b0fe2fc5cad
Felix Ernst <fe.a.ernst@gmail.com>
2021-06-12 16:38:26 +0200

    Relicence files to LGPL-2.1-or-later
    The licence of Gwenview is probably supposed to be GPL-2.0-or-later
    but this was ignored in four files that were recently added.
    
    This commit changes the licences for these four files. Permission
    of the copyright holders was given before this commit was merged.

--- app/alignwithsidebarwidgetaction.h
+++ app/alignwithsidebarwidgetaction.h
@@ -2,7 +2,7 @@
     This file is part of the KDE project
     SPDX-FileCopyrightText: 2021 Felix Ernst <fe.a.ernst@gmail.com>
 
-    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+    SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
 #ifndef ALIGNWITHSIDEBARWIDGETACTION_H
--- lib/documentview/alphabackgrounditem.h
+++ lib/documentview/alphabackgrounditem.h
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2021 Arjen Hiemstra <ahiemstra@heimr.nl>
  * 
- * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #ifndef BACKGROUNDITEM_H
