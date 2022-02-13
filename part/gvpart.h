2e29dedf51cd65fc217479d77f9802902f56f17f
Laurent Montel <montel@kde.org>
2022-02-13 13:39:11 +0100

    Remove unused forward declaration

--- part/gvpart.h
+++ part/gvpart.h
@@ -28,7 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // Local
 #include <lib/document/document.h>
 
-class KAboutData;
 class KPluginMetaData;
 
 namespace Gwenview
