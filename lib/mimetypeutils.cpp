fbf82788e1ad2acdab73a5e34ed6321f79d41123
Laurent Montel <montel@kde.org>
2022-05-09 06:48:17 +0200

    GIT_SILENT: remove duplicate header between .cpp/.h

--- lib/mimetypeutils.cpp
+++ lib/mimetypeutils.cpp
@@ -27,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <QImageReader>
 #include <QMimeData>
 #include <QMimeDatabase>
-#include <QStringList>
 #include <QUrl>
 
 // KF
