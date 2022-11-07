f72171aa83a237dc0846bf1b9eedf852341dded2
Ilya Pominov <ipominov@astralinux.ru>
2022-11-07 15:20:29 +0300

    Fix license headers in lib/bcg/imageutils.*

--- lib/bcg/imageutils.cpp
+++ lib/bcg/imageutils.cpp
@@ -1,6 +1,6 @@
 /*
-Gwenview - A simple image viewer for KDE
-Copyright 2000-2004 Aurlien Gteau
+Gwenview: an image viewer
+Copyright 2000-2004 Aurélien Gâteau <agateau@kde.org>
 Copyright 2022 Ilya Pominov <ipominov@astralinux.ru>
 
 This program is free software; you can redistribute it and/or
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 */
 #include "imageutils.h"
