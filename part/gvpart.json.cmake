8471a1801682b3417c8e291da9f277d472225121
l10n daemon script <scripty@kde.org>
2023-03-22 01:46:56 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- part/gvpart.json.cmake
+++ part/gvpart.json.cmake
@@ -6,6 +6,16 @@
             "${IMAGE_MIME_LIST}"
         ],
         "Name": "Gwenview Image Viewer",
+        "Name[ca]": "Visualitzador d'imatges Gwenview",
+        "Name[cs]": "Prohlížeč obrázků Gwenview",
+        "Name[es]": "Visor de imágenes Gwenview",
+        "Name[ka]": "Gwenview - სურათების დამთვალიერებელი",
+        "Name[nl]": "GWenview Afbeeldingenviewer",
+        "Name[pt]": "Visualizador de Imagens Gwenview",
+        "Name[pt_BR]": "Visualizador de imagens do Gwenview",
+        "Name[tr]": "Gwenview Görsel Görüntüleyici",
+        "Name[uk]": "Переглядач зображень Gwenview",
+        "Name[x-test]": "xxGwenview Image Viewerxx",
         "ServiceTypes": [
             "KParts/ReadOnlyPart"
         ]
