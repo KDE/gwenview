4ed716826b0031ed109e259571b2ba64da729f71
l10n daemon script <scripty@kde.org>
2023-11-04 11:45:13 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- part/gvpart.json.cmake
+++ part/gvpart.json.cmake
@@ -37,7 +37,6 @@
         "Name[ta]": "குவென்வியூ படக் காட்டி",
         "Name[tr]": "Akbakış Görsel Görüntüleyicisi",
         "Name[uk]": "Переглядач зображень Gwenview",
-        "Name[x-test]": "xxGwenview Image Viewerxx",
         "Name[zh_CN]": "Gwenview 图像查看器",
         "Name[zh_TW]": "Gwenview 影像檢視程式",
         "ServiceTypes": [
