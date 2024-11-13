KDE/gwenview

commit 33d6203c7b4de83a8a1ea92dfc7036af87eb43f2

Convert image to original format to enable correct color correction
QImage::scaled() may unexpectedly change image’s format to Premultiplied,
we need to ensure that image remains in original format supported by
color correction engine.

lib/documentview/rasterimageitem.cpp
@@ -114,6 +114,8 @@ void RasterImageItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * 
         image = mSixthScaledImage.copy(sourceRect);
     }
 
+    const QImage::Format originalImageFormat = image.format();
+
     // We want nearest neighbour when zooming in since that provides the most
     // accurate representation of pixels, but when zooming out it will actually
     // not look very nice, so use smoothing when zooming out.
@@ -121,6 +123,12 @@ void RasterImageItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * 
 
     // Scale the visible image to the requested zoom.
     image = image.scaled(image.size() * targetZoom, Qt::IgnoreAspectRatio, transformationMode);
+
+    // Scaling may convert image to premultiplied formats (unsupported by color correction engine),
+    // so we convert image back to originalImageFormat.
+    if (image.format() != originalImageFormat) {
+        image.convertTo(originalImageFormat);
+    }
 
     // Perform color correction on the visible image.
     applyDisplayTransform(image);

commit 3d52a2694314629491e0b3720608f349978ea8de

Fix garbled colors when scaling indexed-color images
Fixes bug where indexed-color or monochrome-palette images (eg. from
pngquant) would render with garbled colors or B&W noise when zoomed.

I converted images from indexed colors to (A)RGB32, to avoid error
"Gwenview cannot apply color profile on QImage::... images" (depending
on zoom level).

I verified that wide-gamut images (Webkit-logo-P3.png) are still clipped
to SDR gamut. I tested that wide-gamut indexed images are now properly
clipped, but these images are probably ahistorical.

Bug introduced by commit 33d6203c7b4de83a8a1ea92dfc7036af87eb43f2.
BUG: 459627

lib/documentview/rasterimageitem.cpp
@@ -117,6 +117,8 @@ void RasterImageItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * 
     }
 
     const QImage::Format originalImageFormat = image.format();
+    const bool isIndexedColor = !image.colorTable().isEmpty();
+    const bool hasAlphaChannel = image.hasAlphaChannel();
 
     // We want nearest neighbour at high zoom since that provides the most
     // accurate representation of pixels, but at low zoom or when zooming out it
@@ -127,9 +129,13 @@ void RasterImageItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * 
     // Scale the visible image to the requested zoom.
     image = image.scaled(image.size() * targetZoom, Qt::IgnoreAspectRatio, transformationMode);
 
-    // Scaling may convert image to premultiplied formats (unsupported by color correction engine),
-    // so we convert image back to originalImageFormat.
-    if (image.format() != originalImageFormat) {
+    // If the original format is indexed, convert to Format_(A)RGB32 (the default format for loading PNG files).
+    // (ARGB32 is necessary for pngquant'd transparent images to show Gwenview's background color.)
+    // Otherwise scaling may convert image to premultiplied formats (unsupported by color correction engine).
+    // Pray originalImageFormat is not premultiplied, and convert image back to it.
+    if (isIndexedColor) {
+        image.convertTo(hasAlphaChannel ? QImage::Format_ARGB32 : QImage::Format_RGB32);
+    } else if (image.format() != originalImageFormat) {
         image.convertTo(originalImageFormat);
     }
 