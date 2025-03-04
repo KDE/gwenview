KDE/gwenview

commit 3663c774ca0b766583ee92d9b2776d67cf5d5c2a

Improve i18n of percentage and dimension values
Enables i18n for the remaining width-height and percent values.

BUG: 473636
BUG: 473638

lib/documentview/documentview.cpp
@@ -256,10 +256,12 @@ struct DocumentViewPrivate {
         caption = doc->url().fileName();
         QSize size = doc->size();
         if (size.isValid()) {
-            caption += QStringLiteral(" - %1x%2").arg(size.width()).arg(size.height());
+            caption += QStringLiteral(" - ");
+            caption += i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", size.width(), size.height());
             if (mAdapter->canZoom()) {
                 int intZoom = qRound(mAdapter->zoom() * 100);
-                caption += QStringLiteral(" - %1%").arg(intZoom);
+                caption += QStringLiteral(" - ");
+                caption += i18nc("Percent value", "%1%", intZoom);
             }
         }
         Q_EMIT q->captionUpdateRequested(caption);

lib/thumbnailview/previewitemdelegate.cpp
@@ -41,6 +41,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 // KF
 #include <KDirModel>
 #include <KIconLoader>
+#include <KLocalizedString>
 
 #ifndef GWENVIEW_SEMANTICINFO_BACKEND_NONE
 #include <KRatingPainter>
@@ -408,7 +409,7 @@ struct PreviewItemDelegatePrivate {
             QSize fullSize;
             QPixmap thumbnailPix = mView->thumbnailForIndex(index, &fullSize);
             if (fullSize.isValid()) {
-                const QString text = QStringLiteral("%1x%2").arg(fullSize.width()).arg(fullSize.height());
+                const QString text = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", fullSize.width(), fullSize.height());
                 elided |= isTextElided(text);
                 textList << text;
             }
@@ -816,7 +817,7 @@ void PreviewItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &o
 
     if (!isDirOrArchive && (d->mDetails & PreviewItemDelegate::ImageSizeDetail)) {
         if (fullSize.isValid()) {
-            const QString text = QStringLiteral("%1x%2").arg(fullSize.width()).arg(fullSize.height());
+            const QString text = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", fullSize.width(), fullSize.height());
             d->drawText(painter, textRect, fgColor, text);
             textRect.moveTop(textRect.bottom());
         }

lib/documentview/documentviewcontroller.cpp
@@ -87,7 +87,7 @@ struct DocumentViewControllerPrivate {
         mActualSizeAction = view->addAction(KStandardAction::ActualSize);
         mActualSizeAction->setCheckable(true);
         mActualSizeAction->setIcon(QIcon::fromTheme(QStringLiteral("zoom-original")));
-        mActualSizeAction->setIconText(QLocale().toString(100).append(QLocale().percent()));
+        mActualSizeAction->setIconText(i18nc("Original image size percent value", "100%"));
 
         mZoomInAction = view->addAction(KStandardAction::ZoomIn);
         mZoomOutAction = view->addAction(KStandardAction::ZoomOut);

lib/zoomcombobox/zoomcombobox.cpp
@@ -4,6 +4,7 @@
 #include "zoomcombobox.h"
 #include "zoomcombobox_p.h"
 
+#include <KLocalizedString>
 #include <QAbstractItemView>
 #include <QAction>
 #include <QEvent>
@@ -294,8 +295,13 @@ qreal ZoomComboBox::valueFromText(const QString &text, bool *ok) const
     const QLocale l = locale();
     QString s = text;
     s.remove(l.groupSeparator());
+
     if (s.endsWith(l.percent())) {
         s = s.chopped(1);
+    }
+
+    if (s.startsWith(l.percent())) {
+        s = s.remove(0, 1);
     }
 
     return l.toDouble(s, ok) / 100.0;
@@ -308,7 +314,8 @@ QString ZoomComboBox::textFromValue(const qreal value) const
     QLocale l = locale();
     l.setNumberOptions(QLocale::OmitGroupSeparator);
 
-    return l.toString(qRound(value * 100)).append(l.percent());
+    QString formattedValue = l.toString(qRound(value * 100));
+    return i18nc("Percent value", "%1%", formattedValue);
 }
 
 void ZoomComboBox::updateDisplayedText()

commit 1a8894535ec18df43c7d83efaf740d822cf0091b

Fix presentation of image size integers
Changes in 3663c774ca0b766583ee92d9b2776d67cf5d5c2a (bug 473638) caused
image dimensions to be displayed with integer grouping (1,024x1,024)
instead of the more conventional "1024x1024".

BUG: 488639

lib/documentview/documentview.cpp
@@ -257,7 +257,8 @@ struct DocumentViewPrivate {
         QSize size = doc->size();
         if (size.isValid()) {
             caption += QStringLiteral(" - ");
-            caption += i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", size.width(), size.height());
+            caption += i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", QString::number(size.width()), QString::number(size.height()));
+
             if (mAdapter->canZoom()) {
                 int intZoom = qRound(mAdapter->zoom() * 100);
                 caption += QStringLiteral(" - ");

lib/imagemetainfomodel.cpp
@@ -437,7 +437,7 @@ void ImageMetaInfoModel::setImageSize(const QSize &size)
 {
     QString imageSize;
     if (size.isValid()) {
-        imageSize = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", size.width(), size.height());
+        imageSize = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", QString::number(size.width()), QString::number(size.height()));
 
         double megaPixels = size.width() * size.height() / 1000000.;
         if (megaPixels > 0.1) {

lib/thumbnailview/previewitemdelegate.cpp
@@ -412,7 +412,10 @@ struct PreviewItemDelegatePrivate {
             QSize fullSize;
             QPixmap thumbnailPix = mView->thumbnailForIndex(index, &fullSize);
             if (fullSize.isValid()) {
-                const QString text = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", fullSize.width(), fullSize.height());
+                const QString text = i18nc("@item:intable %1 is image width, %2 is image height",
+                                           "%1x%2",
+                                           QString::number(fullSize.width()),
+                                           QString::number(fullSize.height()));
                 elided |= isTextElided(text);
                 textList << text;
             }
@@ -826,7 +829,8 @@ void PreviewItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &o
 
     if (!isDirOrArchive && (d->mDetails & PreviewItemDelegate::ImageSizeDetail)) {
         if (fullSize.isValid()) {
-            const QString text = i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", fullSize.width(), fullSize.height());
+            const QString text =
+                i18nc("@item:intable %1 is image width, %2 is image height", "%1x%2", QString::number(fullSize.width()), QString::number(fullSize.height()));
             d->drawText(painter, textRect, fgColor, text);
             textRect.moveTop(textRect.bottom());
         }